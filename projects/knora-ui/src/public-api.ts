935819aa0fb3db2c6b7164753965e8e0bfe74082
Tobias Schweizer <t.schweizer@unibas.ch>
2020-01-29 11:47:14 +0100

    feature (core): add core module

--- projects/knora-ui/src/public-api.ts
+++ projects/knora-ui/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of knora-ui
  */
 
-export * from './lib/knora-ui.service';
-export * from './lib/knora-ui.component';
-export * from './lib/knora-ui.module';
+export * from './lib/core/index';
