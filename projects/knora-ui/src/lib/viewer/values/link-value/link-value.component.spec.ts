937a3f555f42b7e0d370be412f143fa2473419be
Sepideh Alassi <sepideh.alassi@gmail.com>
2020-02-17 15:41:19 +0100

    feature (value): initialize link value component

---
+++ projects/knora-ui/src/lib/viewer/values/link-value/link-value.component.spec.ts
@@ -0,0 +1,117 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LinkValueComponent } from './link-value.component';
+import { ReadLinkValue, MockResource, UpdateValue, UpdateLinkValue, CreateLinkValue } from '@knora/api';
+import { OnInit, Component, ViewChild, DebugElement } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { $ } from 'protractor';
+import { By } from '@angular/platform-browser';
+
+/**
+ * Test host component to simulate parent component.
+ */
+@Component({
+  template: `
+    <kui-link-value #inputVal [displayValue]="displayInputVal" [mode]="mode"></kui-link-value>`
+})
+class TestHostDisplayValueComponent implements OnInit {
+
+  @ViewChild('inputVal', {static: false}) inputValueComponent: LinkValueComponent;
+
+  displayInputVal: ReadLinkValue;
+
+  mode: 'read' | 'update' | 'create' | 'search';
+
+  ngOnInit() {
+
+    MockResource.getTestthing().subscribe(res => {
+      const inputVal: ReadLinkValue =
+        res[0].getValuesAs('http://0.0.0.0:3333/ontology/0001/anything/v2#hasOtherThingValue', ReadLinkValue)[0];
+
+      this.displayInputVal = inputVal;
+
+      this.mode = 'read';
+    });
+
+  }
+}
+
+/**
+ * Test host component to simulate parent component.
+ */
+@Component({
+  template: `
+    <kui-link-value #inputVal [mode]="mode"></kui-link-value>`
+})
+class TestHostCreateValueComponent implements OnInit {
+
+  @ViewChild('inputVal', {static: false}) inputValueComponent: LinkValueComponent;
+
+  mode: 'read' | 'update' | 'create' | 'search';
+
+  ngOnInit() {
+
+    this.mode = 'create';
+
+  }
+}
+
+describe('LinkValueComponent', () => {
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        LinkValueComponent,
+        TestHostDisplayValueComponent,
+        TestHostCreateValueComponent
+       ],
+       imports: [
+        ReactiveFormsModule,
+        MatInputModule,
+        BrowserAnimationsModule
+      ],
+    })
+    .compileComponents();
+  }));
+
+  describe('display and edit a link value', () => {
+    let testHostComponent: TestHostDisplayValueComponent;
+    let testHostFixture: ComponentFixture<TestHostDisplayValueComponent>;
+    let valueComponentDe: DebugElement;
+    let valueInputDebugElement: DebugElement;
+    let valueInputNativeElement;
+    let commentInputDebugElement: DebugElement;
+    let commentInputNativeElement;
+
+    beforeEach(() => {
+      testHostFixture = TestBed.createComponent(TestHostDisplayValueComponent);
+      testHostComponent = testHostFixture.componentInstance;
+      testHostFixture.detectChanges();
+
+      expect(testHostComponent).toBeTruthy();
+      expect(testHostComponent.inputValueComponent).toBeTruthy();
+
+      const hostCompDe = testHostFixture.debugElement;
+      valueComponentDe = hostCompDe.query(By.directive(LinkValueComponent));
+      valueInputDebugElement = valueComponentDe.query(By.css('input.value'));
+      valueInputNativeElement = valueInputDebugElement.nativeElement;
+
+      commentInputDebugElement = valueComponentDe.query(By.css('input.comment'));
+      commentInputNativeElement = commentInputDebugElement.nativeElement;
+    });
+
+    it('should display an existing value', () => {
+
+      expect(testHostComponent.inputValueComponent.displayValue.linkedResourceIri).toMatch('http://rdfh.ch/0001/0C-0L1kORryKzJAJxxRyRQ');
+
+      expect(testHostComponent.inputValueComponent.form.valid).toBeTruthy();
+
+      expect(testHostComponent.inputValueComponent.mode).toEqual('read');
+
+      expect(valueInputNativeElement.readOnly).toEqual(true);
+
+    });
+  });
+});
---
+++ projects/knora-ui/src/lib/viewer/values/link-value/link-value.component.ts
@@ -0,0 +1,99 @@
+import {Component, Inject, Input, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
+import {BaseValueComponent} from '../base-value.component';
+import {CreateLinkValue, ReadLinkValue, UpdateLinkValue} from '@knora/api';
+import {Subscription} from 'rxjs';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'kui-link-value',
+  templateUrl: './link-value.component.html',
+  styleUrls: ['./link-value.component.scss']
+})
+export class LinkValueComponent extends BaseValueComponent implements OnInit, OnChanges, OnDestroy {
+
+  @Input() displayValue?: ReadLinkValue;
+
+  valueFormControl: FormControl;
+  commentFormControl: FormControl;
+
+  form: FormGroup;
+
+  valueChangesSubscription: Subscription;
+  constructor(@Inject(FormBuilder) private fb: FormBuilder) {
+    super();
+  }
+
+  getInitValue(): string | null {
+    if (this.displayValue !== undefined) {
+      return this.displayValue.linkedResourceIri;
+    } else {
+      return null;
+    }
+  }
+
+  ngOnInit() {
+    // initialize form control elements
+    this.valueFormControl = new FormControl(null);
+
+    this.commentFormControl = new FormControl(null);
+
+    // subscribe to any change on the comment and recheck validity
+    this.valueChangesSubscription = this.commentFormControl.valueChanges.subscribe(
+      data => {
+        this.valueFormControl.updateValueAndValidity();
+      }
+    );
+
+    this.form = this.fb.group({
+      linkValue: this.valueFormControl,
+      comment: this.commentFormControl
+    });
+
+    this.resetFormControl();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.resetFormControl();
+  }
+
+  // unsubscribe when the object is destroyed to prevent memory leaks
+  ngOnDestroy(): void {
+    this.unsubscribeFromValueChanges();
+  }
+
+  getNewValue(): CreateLinkValue | false {
+    if (this.mode !== 'create' || !this.form.valid) {
+      return false;
+    }
+
+    const newLinkValue = new CreateLinkValue();
+
+    newLinkValue.linkedResourceIri = this.valueFormControl.value;
+
+    if (this.commentFormControl.value !== null && this.commentFormControl.value !== '') {
+      newLinkValue.valueHasComment = this.commentFormControl.value;
+    }
+
+    return newLinkValue;
+  }
+
+  getUpdatedValue(): UpdateLinkValue | false {
+    if (this.mode !== 'update' || !this.form.valid) {
+      return false;
+    }
+
+    const updatedLinkValue = new UpdateLinkValue();
+
+    updatedLinkValue.id = this.displayValue.id;
+
+    updatedLinkValue.linkedResourceIri = this.valueFormControl.value;
+
+    // add the submitted comment to updatedIntValue only if user has added a comment
+    if (this.commentFormControl.value !== null && this.commentFormControl.value !== '') {
+      updatedLinkValue.valueHasComment = this.commentFormControl.value;
+    }
+
+    return updatedLinkValue;
+  }
+
+}
