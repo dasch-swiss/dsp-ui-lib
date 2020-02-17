548d08db7f6231053f9dd7eb1faa79952e79a94d
Tobias Schweizer <t.schweizer@unibas.ch>
2020-02-17 17:13:38 +0100

    Revert "test (lib): automatically look for deactivated tests"
    This reverts commit 744baebae9fcdade56885c183902bf71d5033c46.

--- find-ignored-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# find deactivated tests
-hits=`find projects/knora-ui -name "*.spec.ts" | xargs grep 'fit\|fdescribe\|xit' | wc -l`
-if [ $hits -ne 0 ];
- then
-  echo "Deactivated tests found: ", `find projects/knora-ui -name "*.spec.ts" | xargs grep 'fit\|fdescribe\|xit'`
-  exit 1
-fi
