729f1902d2b0c81aeb943159ea587679cf59df04
Tobias Schweizer <t.schweizer@unibas.ch>
2020-02-17 16:26:52 +0100

    test (input): add test for interval input component

--- projects/knora-ui/src/lib/viewer/values/interval-value/interval-input/interval-input.component.spec.ts
+++ projects/knora-ui/src/lib/viewer/values/interval-value/interval-input/interval-input.component.spec.ts
@@ -1,25 +1,62 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { IntervalInputComponent } from './interval-input.component';
+import {Interval, IntervalInputComponent} from './interval-input.component';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {Component, OnInit, ViewChild} from "@angular/core";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+/**
+ * Test host component to simulate parent component.
+ */
+@Component({
+  template: `
+    <div [formGroup]="form">
+    <mat-form-field>
+        <kui-interval-input #intervalInput [formControlName]="'interval'"></kui-interval-input>
+    </mat-form-field>
+    </div>`
+})
+class TestHostComponent implements OnInit {
+
+  @ViewChild('intervalInput', {static: false}) intervalInputComponent: IntervalInputComponent;
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+
+    this.form = this.fb.group({
+      interval: [new Interval(1, 2)]
+    });
+
+  }
+}
 
 describe('InvertalInputComponent', () => {
-  let component: IntervalInputComponent;
-  let fixture: ComponentFixture<IntervalInputComponent>;
+  let testHostComponent: TestHostComponent;
+  let testHostFixture: ComponentFixture<TestHostComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ IntervalInputComponent ]
+      imports: [ReactiveFormsModule, MatFormFieldModule, BrowserAnimationsModule],
+      declarations: [IntervalInputComponent, TestHostComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(IntervalInputComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    testHostFixture = TestBed.createComponent(TestHostComponent);
+    testHostComponent = testHostFixture.componentInstance;
+    testHostFixture.detectChanges();
+
+    expect(testHostComponent).toBeTruthy();
+    expect(testHostComponent.intervalInputComponent).toBeTruthy();
   });
 
-  xit('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create', () => {
+    expect(testHostComponent.intervalInputComponent).toBeTruthy();
   });
 });
