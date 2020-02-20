1fb58325eadc1d30405dfc3b94910b69f6b58712
flaurens <flavie.laurens@unibas.ch>
2020-02-20 14:51:05 +0100

    fix(operations): Add missing comma

--- projects/knora-ui/src/lib/viewer/operations/display-edit/display-edit.component.spec.ts
+++ projects/knora-ui/src/lib/viewer/operations/display-edit/display-edit.component.spec.ts
@@ -1,7 +1,7 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {DisplayEditComponent} from './display-edit.component';
-import {Component, Inject, Input, OnInit, ViewChild} from '@angular/core';
+import { DisplayEditComponent } from './display-edit.component';
+import { Component, Inject, Input, OnInit, ViewChild } from '@angular/core';
 import {
   KnoraApiConnection,
   MockResource,
@@ -14,11 +14,11 @@ import {
   UpdateDecimalValue
 } from '@knora/api';
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {By} from '@angular/platform-browser';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {of} from 'rxjs';
-import {KnoraApiConnectionToken} from '../../../core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { of } from 'rxjs';
+import { KnoraApiConnectionToken } from '../../../core';
 
 @Component({
   selector: `kui-text-value-as-string`,
@@ -95,7 +95,7 @@ class TestDecimalValueComponent implements OnInit {
       test: new FormControl(null, [Validators.required])
     });
   }
-  
+
   getUpdatedValue(): UpdateValue {
     const updateDecimalVal = new UpdateDecimalValue();
 
@@ -116,7 +116,7 @@ class TestDecimalValueComponent implements OnInit {
 })
 class TestHostDisplayValueComponent implements OnInit {
 
-  @ViewChild('displayEditVal', {static: false}) displayEditValueComponent: DisplayEditComponent;
+  @ViewChild('displayEditVal', { static: false }) displayEditValueComponent: DisplayEditComponent;
 
   readResource: ReadResource;
   readValue: ReadValue;
@@ -161,7 +161,7 @@ describe('DisplayEditComponent', () => {
         TestHostDisplayValueComponent,
         TestTextValueAsStringComponent,
         TestIntValueComponent,
-        TestBooleanValueComponent
+        TestBooleanValueComponent,
         TestDecimalValueComponent
       ],
       providers: [
