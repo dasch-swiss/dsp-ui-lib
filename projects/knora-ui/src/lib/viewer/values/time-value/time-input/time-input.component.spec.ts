e533767a1b29f68eb69609d9ea407d17ee1c07a2
mdelez <60604010+mdelez@users.noreply.github.com>
2020-03-02 14:54:31 +0100

    test (value): implementing unit tests

--- projects/knora-ui/src/lib/viewer/values/time-value/time-input/time-input.component.spec.ts
+++ projects/knora-ui/src/lib/viewer/values/time-value/time-input/time-input.component.spec.ts
@@ -1,25 +1,122 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TimeInputComponent } from './time-input.component';
+import { DateTime, TimeInputComponent } from './time-input.component';
+import { KnoraDate } from '@knora/api';
+import { Component, OnInit, ViewChild, DebugElement } from '@angular/core';
+import { FormGroup, FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule, MatInputModule, MatNativeDateModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
+import { MatDatepickerModule } from '@angular/material/datepicker'
+
+
+/**
+ * Test host component to simulate parent component.
+ */
+@Component({
+  template: `
+    <div [formGroup]="form">
+      <mat-form-field>
+        <kui-time-input #timeInput [formControlName]="'time'" [readonly]="readonly"></kui-time-input>
+      </mat-form-field>
+    </div>`
+})
+class TestHostComponent implements OnInit {
+
+  @ViewChild('timeInput', {static: false}) timeInputComponent: TimeInputComponent;
+
+  form: FormGroup;
+
+  readonly = false;
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+
+    this.form = this.fb.group({
+      time: [new DateTime(new KnoraDate("Gregorian", "AD", 1993, 10, 10), "11:45")]
+    });
+
+    console.log('form.time: ',this.form.get('time').value);
+
+  }
+}
 
 describe('TimeInputComponent', () => {
-  let component: TimeInputComponent;
-  let fixture: ComponentFixture<TimeInputComponent>;
+  let testHostComponent: TestHostComponent;
+  let testHostFixture: ComponentFixture<TestHostComponent>;
+
+  let datetimeInputComponentDe: DebugElement;
+  let dateInputDebugElement: DebugElement;
+  let dateInputNativeElement;
+  let timeInputDebugElement: DebugElement;
+  let timeInputNativeElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TimeInputComponent ]
+      imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, BrowserAnimationsModule],
+      declarations: [TimeInputComponent, TestHostComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TimeInputComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    testHostFixture = TestBed.createComponent(TestHostComponent);
+    testHostComponent = testHostFixture.componentInstance;
+    testHostFixture.detectChanges();
+
+    expect(testHostComponent).toBeTruthy();
+    expect(testHostComponent.timeInputComponent).toBeTruthy();
+    expect(testHostComponent.timeInputComponent.readonly).toBeFalsy();
+
+    const hostCompDe = testHostFixture.debugElement;
+    datetimeInputComponentDe = hostCompDe.query(By.directive(TimeInputComponent));
+
+    dateInputDebugElement = datetimeInputComponentDe.query(By.css('input.date'));
+    console.log('dateInputDebugElement: ', dateInputDebugElement);
+    dateInputNativeElement = dateInputDebugElement.nativeElement;
+    //console.log('dateInputNativeElement: ', dateInputNativeElement);
+
+    timeInputDebugElement = datetimeInputComponentDe.query(By.css('input.time'));
+    //console.log('timeInputDebugElement: ', timeInputDebugElement);
+    timeInputNativeElement = timeInputDebugElement.nativeElement;
+
+    expect(dateInputNativeElement.readOnly).toBeFalsy;
+    expect(timeInputNativeElement.readOnly).toBeFalsy;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should initialize the DateTime correctly', () => {
+
+    //dateInputNativeElement.value = "10-10-1993";
+
+    //console.log('dateInputNativeElement.value: ', dateInputNativeElement.value);
+    //console.log('timeInputNativeElement.value: ', timeInputNativeElement.value);
+    //expect(dateInputNativeElement.value).toEqual("10-10-1993");
+    expect(timeInputNativeElement.value).toEqual('11:45');
+
+  });
+
+  it('should propagate changes made by the user', () => {
+
+    dateInputNativeElement.value = '11-12-1993';
+    dateInputNativeElement.dispatchEvent(new Event('dateChange'));
+
+    testHostFixture.detectChanges();
+
+    expect(testHostComponent.form.controls.time).toBeTruthy();
+    // expect(testHostComponent.form.controls.time.value.date.year).toEqual(1993);
+    // expect(testHostComponent.form.controls.time.value.date.month).toEqual(12);
+    // expect(testHostComponent.form.controls.time.value.date.day).toEqual(11);
+    expect(testHostComponent.form.controls.time.value.time).toEqual('11:45');
+
+    timeInputNativeElement.value = '10:35';
+    timeInputNativeElement.dispatchEvent(new Event('input'));
+
+    testHostFixture.detectChanges();
+
+    expect(testHostComponent.form.controls.time.value).toBeTruthy();
+    expect(testHostComponent.form.controls.time.value.time).toEqual('10:35');
+
   });
 });
--- projects/knora-ui/src/lib/viewer/values/time-value/time-value.component.spec.ts
+++ projects/knora-ui/src/lib/viewer/values/time-value/time-value.component.spec.ts
@@ -1,87 +1,173 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { TimeValueComponent } from './time-value.component';
-import { Component, OnInit, ViewChild, DebugElement } from '@angular/core';
-import { TimeInputComponent, DateTime } from './time-input/time-input.component';
-import { FormGroup, FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { KnoraDate } from '@knora/api';
-import { MatFormFieldModule, MatInputModule } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { By } from '@angular/platform-browser';
+import {TimeValueComponent} from './time-value.component';
+import {Component, DebugElement, forwardRef, Input, OnInit, ViewChild} from '@angular/core';
+import {CreateTimeValue, MockResource, ReadTimeValue, UpdateTimeValue, KnoraDate} from '@knora/api';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR, NgControl, ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {DateTime} from './time-input/time-input.component';
+import {MatFormFieldControl} from '@angular/material/form-field';
+import {Subject} from 'rxjs';
+import {By} from '@angular/platform-browser';
+import {ErrorStateMatcher} from '@angular/material';
+
+@Component({
+  selector: `kui-time-input`,
+  template: ``,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => TestTimeInputComponent),
+    },
+    {provide: MatFormFieldControl, useExisting: TestTimeInputComponent}
+  ]
+})
+class TestTimeInputComponent implements ControlValueAccessor, MatFormFieldControl<any> {
+
+  @Input() readonly = false;
+  @Input() value;
+  @Input() disabled: boolean;
+  @Input() empty: boolean;
+  @Input() placeholder: string;
+  @Input() required: boolean;
+  @Input() shouldLabelFloat: boolean;
+  @Input() errorStateMatcher: ErrorStateMatcher;
+
+  errorState = false;
+  focused = false;
+  id = 'testid';
+  ngControl: NgControl | null;
+  onChange = (_: any) => {
+  };
+  stateChanges = new Subject<void>();
+
+  writeValue(dateTime: DateTime | null): void {
+    this.value = dateTime;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+  }
+
+  onContainerClick(event: MouseEvent): void {
+  }
+
+  setDescribedByIds(ids: string[]): void {
+  }
+
+  _handleInput(): void {
+    this.onChange(this.value);
+  }
+
+}
 
 /**
  * Test host component to simulate parent component.
  */
 @Component({
   template: `
-    <div [formGroup]="form">
-      <mat-form-field>
-        <kui-time-input #intervalInput [formControlName]="'time'" [readonly]="readonly"></kui-time-input>
-      </mat-form-field>
-    </div>`
+    <kui-time-value #inputVal [displayValue]="displayInputVal" [mode]="mode"></kui-time-value>`
 })
-class TestHostComponent implements OnInit {
+class TestHostDisplayValueComponent implements OnInit {
 
-  @ViewChild('timeInput', {static: false}) timeInputComponent: TimeInputComponent;
+  @ViewChild('inputVal', {static: false}) inputValueComponent: TimeValueComponent;
 
-  form: FormGroup;
+  displayInputVal: ReadTimeValue;
 
-  readonly = false;
+  mode: 'read' | 'update' | 'create' | 'search';
 
-  constructor(private fb: FormBuilder) {
-  }
+  ngOnInit() {
 
-  ngOnInit(): void {
+    MockResource.getTestthing().subscribe(res => {
+      const inputVal: ReadTimeValue =
+        res[0].getValuesAs('http://0.0.0.0:3333/ontology/0001/anything/v2#hasTimeStamp', ReadTimeValue)[0];
 
-    this.form = this.fb.group({
-      interval: [new DateTime(new KnoraDate("Gregorian", "AD", 1993, 10, 10), "19:11")]
+      this.displayInputVal = inputVal;
+
+      this.mode = 'read';
     });
 
   }
 }
 
+/**
+ * Test host component to simulate parent component.
+ */
+@Component({
+  template: `
+    <kui-time-value #inputVal [mode]="mode"></kui-time-value>`
+})
+class TestHostCreateValueComponent implements OnInit {
+
+  @ViewChild('inputVal', {static: false}) inputValueComponent: TimeValueComponent;
+
+  mode: 'read' | 'update' | 'create' | 'search';
+
+  ngOnInit() {
+
+    this.mode = 'create';
+  }
+}
+
 describe('TimeValueComponent', () => {
-  let testHostComponent: TestHostComponent;
-  let testHostFixture: ComponentFixture<TestHostComponent>;
-
-  let timeInputComponentDe: DebugElement;
-  let dateInputDebugElement: DebugElement;
-  let dateInputNativeElement;
-  let timeInputDebugElement: DebugElement;
-  let timeInputNativeElement;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, BrowserAnimationsModule],
-      declarations: [TimeInputComponent, TestHostComponent]
+      declarations: [TimeValueComponent, TestHostDisplayValueComponent, TestTimeInputComponent, TestHostCreateValueComponent],
+      imports: [
+        ReactiveFormsModule,
+        MatInputModule,
+        BrowserAnimationsModule
+      ],
     })
       .compileComponents();
   }));
 
-  beforeEach(() => {
-    testHostFixture = TestBed.createComponent(TestHostComponent);
-    testHostComponent = testHostFixture.componentInstance;
-    testHostFixture.detectChanges();
+  describe('display and edit a time value', () => {
+    let testHostComponent: TestHostDisplayValueComponent;
+    let testHostFixture: ComponentFixture<TestHostDisplayValueComponent>;
 
-    expect(testHostComponent).toBeTruthy();
-    expect(testHostComponent.timeInputComponent).toBeTruthy();
-    expect(testHostComponent.timeInputComponent.readonly).toBeFalsy();
+    let valueComponentDe: DebugElement;
+    let commentInputDebugElement: DebugElement;
+    let commentInputNativeElement;
 
-    const hostCompDe = testHostFixture.debugElement;
-    timeInputComponentDe = hostCompDe.query(By.directive(TimeInputComponent));
-    dateInputDebugElement = timeInputComponentDe.query(By.css('input.start'));
-    dateInputNativeElement = dateInputDebugElement.nativeElement;
-    timeInputDebugElement = timeInputComponentDe.query(By.css('input.end'));
-    timeInputNativeElement = timeInputDebugElement.nativeElement;
+    beforeEach(() => {
+      testHostFixture = TestBed.createComponent(TestHostDisplayValueComponent);
+      testHostComponent = testHostFixture.componentInstance;
+      testHostFixture.detectChanges();
 
-    expect(dateInputNativeElement.readOnly).toBe(false);
-    expect(timeInputNativeElement.readOnly).toBe(false);
-  });
+      expect(testHostComponent).toBeTruthy();
+      expect(testHostComponent.inputValueComponent).toBeTruthy();
 
-  it('should initialize the timestamp correctly', () => {
+      const hostCompDe = testHostFixture.debugElement;
 
-    expect(dateInputNativeElement.value).toEqual('1');
-    expect(timeInputNativeElement.value).toEqual('2');
+      valueComponentDe = hostCompDe.query(By.directive(TimeValueComponent));
+      commentInputDebugElement = valueComponentDe.query(By.css('input.comment'));
+      commentInputNativeElement = commentInputDebugElement.nativeElement;
+    });
 
+    it('should display an existing value', () => {
+
+      expect(testHostComponent.inputValueComponent.displayValue.time).toEqual("2019-08-30T10:45:20.173572Z");
+
+      expect(testHostComponent.inputValueComponent.form.valid).toBeTruthy();
+
+      expect(testHostComponent.inputValueComponent.mode).toEqual('read');
+
+      expect(testHostComponent.inputValueComponent.timeInputComponent.readonly).toEqual(true);      
+
+      expect(testHostComponent.inputValueComponent.timeInputComponent.value.date.year).toEqual(2019);
+
+      expect(testHostComponent.inputValueComponent.timeInputComponent.value.date.month).toEqual(8);
+
+      expect(testHostComponent.inputValueComponent.timeInputComponent.value.date.day).toEqual(30);
+      
+      expect(testHostComponent.inputValueComponent.timeInputComponent.value.time).toEqual("12:45");
+
+    });
   });
 });
