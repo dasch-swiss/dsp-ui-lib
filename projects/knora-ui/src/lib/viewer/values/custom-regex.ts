e9cae9fa155c22a02385dc3a6558243525d7b3a1
Flavie L <flavie.laurens@dasch.swiss>
2020-03-11 16:35:55 +0100

    Color value component (#34)
    * feat(values): Initialize color component
    
    * feat(color-value): Implement the color picker from ngx-color-picker package
    
    * feat(color-value): Add label + color regex validators
    
    * fix(color-value): Fix reset
    
    * feat(color-value): Add color picker component to create a custom mat-form field
    
    * feat(color-value): Implement ControlValueAccessor and custom MatFormFieldControl
    
    * feat(color-picker): Fix reset
    
    * feat(color-value): Add mat-error tags in html files + implement matcher in ts
    
    * test(color-value): Add imports and some tests
    
    * test(color-value): Add tests
    
    * test(color-value): Add more tests
    
    * test(color-value): Remove deactivated tests
    
    * refactor(color-value): Clean up tests
    
    * feat(color-value): Fix standard validator + fix edit tests
    
    * test(color-value): Add create value tests
    
    * test(color-picker): Add more tests
    
    * refactor(color-value): Clean up
    
    * refactor(color-value): Clean up scss files
    
    * fix(color-value): Fix displayed error message when the value has not changed
    
    * fix(color-picker): Add missing errorStateMatcher attribute
    
    * feat(color-value): Replace ColorPicker object to manipulate a string. Refactor code accordingly.
    
    * fix(color): Fix the getUpdatedValue and getCreatedValue + update the unit tests
    
    * fix(color-value): Deal with color as string + update tests
    
    * chore: ngx-color-picker v8.2.0
    
    * refactor(color-picker): Clean up
    
    * test(color-value): Fix create value test
    
    * docs(color-picker): Add npm package link in comment
    
    * test(color-picker): Fix test
    
    * tset(color-picker): Update test with disabled testing
    
    Co-authored-by: flaurens <flavie.laurens@unibas.ch>

--- projects/knora-ui/src/lib/viewer/values/custom-regex.ts
+++ projects/knora-ui/src/lib/viewer/values/custom-regex.ts
@@ -1,10 +1,11 @@
 // @dynamic
 // https://github.com/ng-packagr/ng-packagr/issues/641
 
-export class CustomRegex{
+export class CustomRegex {
 
     public static readonly INT_REGEX = /^-?\d+$/;
 
-    public static readonly URI_REGEX = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
-    
+    public static readonly URI_REGEX = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+
+    public static readonly COLOR_REGEX = /^#(?:[0-9a-fA-F]{6})$/;
 }
