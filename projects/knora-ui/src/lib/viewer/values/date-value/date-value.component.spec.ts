da95792d13f8232976e42837432cde66efdb7b7d
Tobias Schweizer <t.schweizer@unibas.ch>
2020-03-17 14:43:02 +0100

    test (value): specs for date value component

--- projects/knora-ui/src/lib/viewer/values/date-value/date-value.component.spec.ts
+++ projects/knora-ui/src/lib/viewer/values/date-value/date-value.component.spec.ts
@@ -1,6 +1,119 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { DateValueComponent } from './date-value.component';
+import {DateValueComponent} from './date-value.component';
+import {Component, DebugElement, forwardRef, Input, OnInit, ViewChild} from '@angular/core';
+import {IntValueComponent} from '../int-value/int-value.component';
+import {KnoraDate, KnoraPeriod, MockResource, ReadDateValue} from '@knora/api';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR, NgControl, ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldControl} from '@angular/material/form-field';
+import {Subject} from 'rxjs';
+import {ErrorStateMatcher} from '@angular/material/core';
+import {By} from '@angular/platform-browser';
+import {MatInputModule} from '@angular/material/input';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+@Component({
+  selector: `kui-date-input`,
+  template: ``,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => TestDateInputComponent),
+    },
+    {provide: MatFormFieldControl, useExisting: TestDateInputComponent}
+  ]
+})
+class TestDateInputComponent implements ControlValueAccessor, MatFormFieldControl<any> {
+
+  @Input() readonly = false;
+  @Input() value;
+  @Input() disabled: boolean;
+  @Input() empty: boolean;
+  @Input() placeholder: string;
+  @Input() required: boolean;
+  @Input() shouldLabelFloat: boolean;
+  @Input() errorStateMatcher: ErrorStateMatcher;
+
+  errorState = false;
+  focused = false;
+  id = 'testid';
+  ngControl: NgControl | null;
+  onChange = (_: any) => {
+  };
+  stateChanges = new Subject<void>();
+
+  writeValue(date: KnoraDate | KnoraPeriod | null): void {
+    this.value = date;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+  }
+
+  onContainerClick(event: MouseEvent): void {
+  }
+
+  setDescribedByIds(ids: string[]): void {
+  }
+
+  _handleInput(): void {
+    this.onChange(this.value);
+  }
+
+}
+
+/**
+ * Test host component to simulate parent component.
+ */
+@Component({
+  template: `
+    <kui-date-value #inputVal [displayValue]="displayInputVal" [mode]="mode"></kui-date-value>`
+})
+class TestHostDisplayValueComponent implements OnInit {
+
+  @ViewChild('inputVal', {static: false}) inputValueComponent: IntValueComponent;
+
+  displayInputVal: ReadDateValue;
+
+  mode: 'read' | 'update' | 'create' | 'search';
+
+  ngOnInit() {
+
+    MockResource.getTestthing().subscribe(res => {
+      const inputVal: ReadDateValue =
+        res[0].getValuesAs('http://0.0.0.0:3333/ontology/0001/anything/v2#hasDate', ReadDateValue)[0];
+
+      this.displayInputVal = inputVal;
+
+      this.mode = 'read';
+    });
+
+  }
+}
+
+/**
+ * Test host component to simulate parent component.
+ */
+@Component({
+  template: `
+    <kui-date-value #inputVal [mode]="mode"></kui-date-value>`
+})
+class TestHostCreateValueComponent implements OnInit {
+
+  @ViewChild('inputVal', {static: false}) inputValueComponent: IntValueComponent;
+
+  mode: 'read' | 'update' | 'create' | 'search';
+
+  ngOnInit() {
+
+    this.mode = 'create';
+
+  }
+}
 
 describe('DateValueComponent', () => {
   let component: DateValueComponent;
@@ -8,9 +121,18 @@ describe('DateValueComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DateValueComponent ]
+      imports: [
+        ReactiveFormsModule,
+        MatInputModule,
+        BrowserAnimationsModule
+      ],
+      declarations: [
+        DateValueComponent,
+        TestDateInputComponent,
+        TestHostDisplayValueComponent,
+        TestHostCreateValueComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +141,32 @@ describe('DateValueComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('display and edit an interval value', () => {
+    let testHostComponent: TestHostDisplayValueComponent;
+    let testHostFixture: ComponentFixture<TestHostDisplayValueComponent>;
+
+    let valueComponentDe: DebugElement;
+    let commentInputDebugElement: DebugElement;
+    let commentInputNativeElement;
+
+    beforeEach(() => {
+      testHostFixture = TestBed.createComponent(TestHostDisplayValueComponent);
+      testHostComponent = testHostFixture.componentInstance;
+      testHostFixture.detectChanges();
+
+      expect(testHostComponent).toBeTruthy();
+      expect(testHostComponent.inputValueComponent).toBeTruthy();
+
+      const hostCompDe = testHostFixture.debugElement;
+
+      valueComponentDe = hostCompDe.query(By.directive(DateValueComponent));
+      commentInputDebugElement = valueComponentDe.query(By.css('input.comment'));
+      commentInputNativeElement = commentInputDebugElement.nativeElement;
+    });
+
+    it('should display an existing value', () => {
+      expect(testHostComponent.inputValueComponent).toBeTruthy();
+      // console.log(testHostComponent.inputValueComponent)
+    });
   });
 });
