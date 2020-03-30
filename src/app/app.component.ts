e3444429d5c90812584a38d8fee502813126d4f6
flaurens <flavie.laurens@unibas.ch>
2020-03-30 14:44:08 +0200

    chore(app): Clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { ApiResponseData, KnoraApiConnection, LoginResponse, ReadResource } from '@knora/api';
-import { KnoraApiConnectionToken } from 'knora-ui';
-import { mergeMap } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -11,26 +8,12 @@ import { mergeMap } from 'rxjs/operators';
 export class AppComponent implements OnInit {
 
   resourceIri: string;
-  // testthing: ReadResource;
 
-  constructor(@Inject(KnoraApiConnectionToken) private knoraApiConnection: KnoraApiConnection) {
+  constructor() {
   }
 
   ngOnInit(): void {
     this.resourceIri = 'http://rdfh.ch/0001/H6gBWUuJSuuO-CilHV8kQw';
-
-    /* this.knoraApiConnection.v2.auth.login('username', 'root', 'test').pipe(
-      mergeMap(
-        (loginResponse: ApiResponseData<LoginResponse>) => {
-          return this.knoraApiConnection.v2.res.getResource('http://rdfh.ch/0001/H6gBWUuJSuuO-CilHV8kQw');
-        }
-      )
-    ).subscribe(
-      (resource: ReadResource) => {
-        this.testthing = resource;
-        this.resourceIri = resource.id;
-      }
-    ); */
   }
 
 }
