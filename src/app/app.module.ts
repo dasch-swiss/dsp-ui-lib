7124071d0b57a7670e88e194a01c49fb1aa3e150
flaurens <flavie.laurens@unibas.ch>
2020-04-16 17:04:14 +0200

    fix(app): Fix viewer name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,52 +1,52 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {APP_INITIALIZER, NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {KnoraApiConfigToken, KnoraApiConnectionToken, KuiConfigToken, ViewerModule} from 'knora-ui';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AppInitService} from './app-init.service';
-import {MatJDNConvertibleCalendarDateAdapterModule} from 'jdnconvertiblecalendardateadapter';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { KnoraApiConfigToken, KnoraApiConnectionToken, KuiConfigToken, KuiViewerModule } from 'knora-ui';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppInitService } from './app-init.service';
+import { MatJDNConvertibleCalendarDateAdapterModule } from 'jdnconvertiblecalendardateadapter';
 
 export function initializeApp(appInitService: AppInitService) {
-  return (): Promise<any> => {
-    return appInitService.Init();
-  };
+    return (): Promise<any> => {
+        return appInitService.Init();
+    };
 }
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    ViewerModule,
-    MatJDNConvertibleCalendarDateAdapterModule
-  ],
-  providers: [
-    AppInitService,
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initializeApp,
-      deps: [AppInitService],
-      multi: true
-    },
-    {
-      provide: KuiConfigToken,
-      useFactory: () => AppInitService.kuiConfig
-    },
-    {
-      provide: KnoraApiConfigToken,
-      useFactory: () => AppInitService.knoraApiConfig
-    },
-    {
-      provide: KnoraApiConnectionToken,
-      useFactory: () => AppInitService.knoraApiConnection
-    }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        KuiViewerModule,
+        MatJDNConvertibleCalendarDateAdapterModule
+    ],
+    providers: [
+        AppInitService,
+        {
+            provide: APP_INITIALIZER,
+            useFactory: initializeApp,
+            deps: [AppInitService],
+            multi: true
+        },
+        {
+            provide: KuiConfigToken,
+            useFactory: () => AppInitService.kuiConfig
+        },
+        {
+            provide: KnoraApiConfigToken,
+            useFactory: () => AppInitService.knoraApiConfig
+        },
+        {
+            provide: KnoraApiConnectionToken,
+            useFactory: () => AppInitService.knoraApiConnection
+        }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
