90b39c6ec6c5b095242bee493ab341a49a4f196d
flaurens <flavie.laurens@unibas.ch>
2020-04-22 11:41:45 +0200

    fix(boolean-value): Remove unused MatCheckboxChange import

--- projects/knora-ui/src/lib/viewer/values/boolean-value/boolean-value.component.ts
+++ projects/knora-ui/src/lib/viewer/values/boolean-value/boolean-value.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, NgZone, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { MatCheckboxChange } from '@angular/material/typings';
 import { CreateBooleanValue, ReadBooleanValue, UpdateBooleanValue } from '@knora/api';
 import { Subscription } from 'rxjs';
 import { BaseValueComponent } from '../base-value.component';
@@ -8,133 +7,133 @@ import { CdkTextareaAutosize } from '@angular/cdk/text-field';
 import { take } from 'rxjs/operators';
 
 @Component({
-  selector: 'kui-boolean-value',
-  templateUrl: './boolean-value.component.html',
-  styleUrls: ['./boolean-value.component.scss']
+    selector: 'kui-boolean-value',
+    templateUrl: './boolean-value.component.html',
+    styleUrls: ['./boolean-value.component.scss']
 })
 export class BooleanValueComponent extends BaseValueComponent implements OnInit, OnChanges, OnDestroy {
 
-  @ViewChild('autosize') autosize: CdkTextareaAutosize;
+    @ViewChild('autosize') autosize: CdkTextareaAutosize;
 
-  @Input() displayValue?: ReadBooleanValue;
+    @Input() displayValue?: ReadBooleanValue;
 
-  valueFormControl: FormControl;
-  commentFormControl: FormControl;
+    valueFormControl: FormControl;
+    commentFormControl: FormControl;
 
-  form: FormGroup;
+    form: FormGroup;
 
-  valueChangesSubscription: Subscription;
+    valueChangesSubscription: Subscription;
 
-  customValidators = [];
+    customValidators = [];
 
-  booleanLabel: string;
+    booleanLabel: string;
 
-  constructor(@Inject(FormBuilder) private fb: FormBuilder, private _ngZone: NgZone) {
-    super();
-  }
-
-  getInitValue(): boolean | null {
-    if (this.displayValue !== undefined) {
-      return this.displayValue.bool;
-    } else {
-      return null;
-    }
-  }
-
-  ngOnInit() {
-    // initialize form control elements
-    this.valueFormControl = new FormControl(null);
-    this.commentFormControl = new FormControl(null);
-
-    // subscribe to any change on the comment and recheck validity
-    this.valueChangesSubscription = this.commentFormControl.valueChanges.subscribe(
-      data => {
-        this.valueFormControl.updateValueAndValidity();
-      }
-    );
-
-    this.form = this.fb.group({
-      booleanValue: this.valueFormControl,
-      comment: this.commentFormControl
-    });
-
-    this.resetFormControl();
-  }
-
-  // override the resetFormControl() from the base component to deal with the disabled state and the checkbox label
-  resetFormControl(): void {
-    super.resetFormControl();
-
-    if (this.valueFormControl !== undefined) {
-      this.setBoolLabel();
-      if (this.mode === 'read') {
-        this.valueFormControl.disable();
-      } else {
-        this.valueFormControl.enable();
-      }
+    constructor(@Inject(FormBuilder) private fb: FormBuilder, private _ngZone: NgZone) {
+        super();
     }
 
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    this.resetFormControl();
-  }
-
-  // unsubscribe when the object is destroyed to prevent memory leaks
-  ngOnDestroy(): void {
-    this.unsubscribeFromValueChanges();
-  }
-
-  getNewValue(): CreateBooleanValue | false {
-    if (this.mode !== 'create' || !this.form.valid) {
-      return false;
+    getInitValue(): boolean | null {
+        if (this.displayValue !== undefined) {
+            return this.displayValue.bool;
+        } else {
+            return null;
+        }
     }
 
-    const newBooleanValue = new CreateBooleanValue();
+    ngOnInit() {
+        // initialize form control elements
+        this.valueFormControl = new FormControl(null);
+        this.commentFormControl = new FormControl(null);
 
-    newBooleanValue.bool = this.valueFormControl.value;
+        // subscribe to any change on the comment and recheck validity
+        this.valueChangesSubscription = this.commentFormControl.valueChanges.subscribe(
+            data => {
+                this.valueFormControl.updateValueAndValidity();
+            }
+        );
 
-    // add the submitted new comment to newBooleanValue only if the user has added a comment
-    if (this.commentFormControl.value !== null && this.commentFormControl.value !== '') {
-      newBooleanValue.valueHasComment = this.commentFormControl.value;
+        this.form = this.fb.group({
+            booleanValue: this.valueFormControl,
+            comment: this.commentFormControl
+        });
+
+        this.resetFormControl();
     }
 
-    return newBooleanValue;
-  }
+    // override the resetFormControl() from the base component to deal with the disabled state and the checkbox label
+    resetFormControl(): void {
+        super.resetFormControl();
 
-  getUpdatedValue(): UpdateBooleanValue | false {
-    if (this.mode !== 'update' || !this.form.valid) {
-      return false;
+        if (this.valueFormControl !== undefined) {
+            this.setBoolLabel();
+            if (this.mode === 'read') {
+                this.valueFormControl.disable();
+            } else {
+                this.valueFormControl.enable();
+            }
+        }
+
     }
 
-    const updatedBooleanValue = new UpdateBooleanValue();
-
-    updatedBooleanValue.id = this.displayValue.id;
-
-    updatedBooleanValue.bool = this.valueFormControl.value;
-
-    // add the submitted comment to updatedBooleanValue only if user has added a comment
-    if (this.commentFormControl.value !== null && this.commentFormControl.value !== '') {
-      updatedBooleanValue.valueHasComment = this.commentFormControl.value;
+    ngOnChanges(changes: SimpleChanges): void {
+        this.resetFormControl();
     }
 
-    return updatedBooleanValue;
-  }
+    // unsubscribe when the object is destroyed to prevent memory leaks
+    ngOnDestroy(): void {
+        this.unsubscribeFromValueChanges();
+    }
 
-  // update dynamically the checkbox label according to the checked status
-  onChecked() {
-    this.setBoolLabel();
-  }
+    getNewValue(): CreateBooleanValue | false {
+        if (this.mode !== 'create' || !this.form.valid) {
+            return false;
+        }
 
-  private setBoolLabel(): string {
-    this.booleanLabel = this.valueFormControl.value ? 'true' : 'false';
-    return this.booleanLabel;
-  }
+        const newBooleanValue = new CreateBooleanValue();
 
-  triggerResize() {
-    // Wait for changes to be applied, then trigger textarea resize.
-    this._ngZone.onStable.pipe(take(1))
-        .subscribe(() => this.autosize.resizeToFitContent(true));
-  }
+        newBooleanValue.bool = this.valueFormControl.value;
+
+        // add the submitted new comment to newBooleanValue only if the user has added a comment
+        if (this.commentFormControl.value !== null && this.commentFormControl.value !== '') {
+            newBooleanValue.valueHasComment = this.commentFormControl.value;
+        }
+
+        return newBooleanValue;
+    }
+
+    getUpdatedValue(): UpdateBooleanValue | false {
+        if (this.mode !== 'update' || !this.form.valid) {
+            return false;
+        }
+
+        const updatedBooleanValue = new UpdateBooleanValue();
+
+        updatedBooleanValue.id = this.displayValue.id;
+
+        updatedBooleanValue.bool = this.valueFormControl.value;
+
+        // add the submitted comment to updatedBooleanValue only if user has added a comment
+        if (this.commentFormControl.value !== null && this.commentFormControl.value !== '') {
+            updatedBooleanValue.valueHasComment = this.commentFormControl.value;
+        }
+
+        return updatedBooleanValue;
+    }
+
+    // update dynamically the checkbox label according to the checked status
+    onChecked() {
+        this.setBoolLabel();
+    }
+
+    private setBoolLabel(): string {
+        this.booleanLabel = this.valueFormControl.value ? 'true' : 'false';
+        return this.booleanLabel;
+    }
+
+    triggerResize() {
+        // Wait for changes to be applied, then trigger textarea resize.
+        this._ngZone.onStable.pipe(take(1))
+            .subscribe(() => this.autosize.resizeToFitContent(true));
+    }
 
 }
