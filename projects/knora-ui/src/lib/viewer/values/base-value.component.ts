23024975d2cd55317903d4b891995d782b9bf06b
Tobias Schweizer <t.schweizer@unibas.ch>
2020-04-28 10:24:31 +0200

    refactor (value components): create value comparison method

--- projects/knora-ui/src/lib/viewer/values/base-value.component.ts
+++ projects/knora-ui/src/lib/viewer/values/base-value.component.ts
@@ -1,4 +1,4 @@
-import { Input, Directive } from '@angular/core';
+import {Input, Directive} from '@angular/core';
 import {CreateValue, ReadValue, UpdateValue} from '@knora/api';
 import {AbstractControl, FormControl, FormGroup, ValidatorFn, Validators} from '@angular/forms';
 import {Subscription} from 'rxjs';
@@ -6,138 +6,157 @@ import {Subscription} from 'rxjs';
 @Directive()
 export abstract class BaseValueComponent {
 
-  shouldShowComment = false;
+    shouldShowComment = false;
 
-  /**
-   * Value to be displayed, if any.
-   */
-  @Input() abstract displayValue?: ReadValue;
+    /**
+     * Value to be displayed, if any.
+     */
+    @Input() abstract displayValue?: ReadValue;
 
-  /**
-   * Sets the mode of the component.
-   */
-  @Input() mode: 'read' | 'update' | 'create' | 'search';
+    /**
+     * Sets the mode of the component.
+     */
+    @Input() mode: 'read' | 'update' | 'create' | 'search';
 
-  /**
-   * FormControl element for the value.
-   */
-  abstract valueFormControl: FormControl;
+    /**
+     * FormControl element for the value.
+     */
+    abstract valueFormControl: FormControl;
 
-  /**
-   * FormControl element for the comment on the value.
-   */
-  abstract commentFormControl: FormControl;
+    /**
+     * FormControl element for the comment on the value.
+     */
+    abstract commentFormControl: FormControl;
 
-  /**
-   * FormGroup that contains FormControl elements.
-   */
-  abstract form: FormGroup;
+    /**
+     * FormGroup that contains FormControl elements.
+     */
+    abstract form: FormGroup;
 
-  /**
-   * Subscription used for when the value changes.
-   */
-  abstract valueChangesSubscription: Subscription;
+    /**
+     * Subscription used for when the value changes.
+     */
+    abstract valueChangesSubscription: Subscription;
 
-  /**
-   * Custom validators for a specific value type.
-   * Can be initialized to an empty array if not needed.
-   */
-  abstract customValidators: ValidatorFn[];
+    /**
+     * Custom validators for a specific value type.
+     * Can be initialized to an empty array if not needed.
+     */
+    abstract customValidators: ValidatorFn[];
 
-  standardValidatorFunc: (val: any, comment: string, commentCtrl: FormControl) => ValidatorFn = (initValue: any, initComment: string, commentFormControl: FormControl): ValidatorFn => {
-    return (control: AbstractControl): { [key: string]: any } | null => {
+    /**
+     * Standard implementation for comparison of primitive values.
+     * Returns true if two values are equal.
+     *
+     * @param initValue Initially given value.
+     * @param curValue Current value.
+     */
+    standardValueComparisonFunc(initValue: any, curValue: any): boolean {
+        return initValue === curValue;
+    }
 
-      const invalid = initValue === control.value
-        && (initComment === commentFormControl.value || (initComment === null && commentFormControl.value === ''));
+    /**
+     * Standard implementation to determine if a value or comment have been changed.
+     *
+     * @param initValue Initially given value.
+     * @param initComment Initially given comment.
+     * @param commentFormControl FormControl of the current comment.
+     */
+    standardValidatorFunc: (val: any, comment: string, commentCtrl: FormControl)
+        => ValidatorFn = (initValue: any, initComment: string, commentFormControl: FormControl): ValidatorFn => {
+        return (control: AbstractControl): { [key: string]: any } | null => {
 
-      return invalid ? {valueNotChanged: {value: control.value}} : null;
+            const invalid = this.standardValueComparisonFunc(initValue, control.value)
+                && (initComment === commentFormControl.value || (initComment === null && commentFormControl.value === ''));
+
+            return invalid ? {valueNotChanged: {value: control.value}} : null;
+        };
     };
-  };
 
-  /**
-   * Returns the initially given value set via displayValue.
-   * Returns null if no value was given.
-   */
-  abstract getInitValue(): any;
+    /**
+     * Returns the initially given value set via displayValue.
+     * Returns null if no value was given.
+     */
+    abstract getInitValue(): any;
 
-  /**
-   * Returns the initially given value comment set via displayValue.
-   * Returns null if no value comment was given.
-   */
-  getInitComment(): string | null {
+    /**
+     * Returns the initially given value comment set via displayValue.
+     * Returns null if no value comment was given.
+     */
+    getInitComment(): string | null {
 
-    if (this.displayValue !== undefined && this.displayValue.valueHasComment !== undefined) {
-      return this.displayValue.valueHasComment;
-    } else {
-      return null;
+        if (this.displayValue !== undefined && this.displayValue.valueHasComment !== undefined) {
+            return this.displayValue.valueHasComment;
+        } else {
+            return null;
+        }
     }
-  }
 
-  /**
-   * Resets the form control elements
-   * with displayValue's value and value comment.
-   * Depending on the mode, validators are reset.
-   */
-  resetFormControl(): void {
-    if (this.valueFormControl !== undefined && this.commentFormControl !== undefined) {
+    /**
+     * Resets the form control elements
+     * with displayValue's value and value comment.
+     * Depending on the mode, validators are reset.
+     */
+    resetFormControl(): void {
+        if (this.valueFormControl !== undefined && this.commentFormControl !== undefined) {
 
-      const initialValue = this.getInitValue();
-      const initialComment = this.getInitComment();
+            const initialValue = this.getInitValue();
+            const initialComment = this.getInitComment();
 
-      this.valueFormControl.setValue(initialValue);
-      this.commentFormControl.setValue(initialComment);
+            this.valueFormControl.setValue(initialValue);
+            this.commentFormControl.setValue(initialComment);
 
-      this.valueFormControl.clearValidators();
+            this.valueFormControl.clearValidators();
 
-      // set validators depending on mode
-      if (this.mode === 'update') {
-        // console.log('reset update validators');
-        this.valueFormControl.setValidators([Validators.required, this.standardValidatorFunc(initialValue, initialComment, this.commentFormControl)].concat(this.customValidators));
-      } else {
-        // console.log('reset read/create validators');
-        this.valueFormControl.setValidators([Validators.required].concat(this.customValidators));
+            // set validators depending on mode
+            if (this.mode === 'update') {
+                // console.log('reset update validators');
+                this.valueFormControl.setValidators([Validators.required, this.standardValidatorFunc(initialValue, initialComment, this.commentFormControl)].concat(this.customValidators));
+            } else {
+                // console.log('reset read/create validators');
+                this.valueFormControl.setValidators([Validators.required].concat(this.customValidators));
 
-      }
+            }
 
-      this.updateCommentVisibility();
-      this.valueFormControl.updateValueAndValidity();
+            this.updateCommentVisibility();
+            this.valueFormControl.updateValueAndValidity();
+        }
     }
-  }
 
-  /**
-   * Unsubscribes from the valueChangesSubscription
-   */
-  unsubscribeFromValueChanges(): void {
-    if (this.valueChangesSubscription !== undefined) {
-      this.valueChangesSubscription.unsubscribe();
+    /**
+     * Unsubscribes from the valueChangesSubscription
+     */
+    unsubscribeFromValueChanges(): void {
+        if (this.valueChangesSubscription !== undefined) {
+            this.valueChangesSubscription.unsubscribe();
+        }
     }
-  }
 
-  /**
-   * Display comment field if in UPDATE or CREATE mode
-   */
-  updateCommentVisibility(): void {
-    this.shouldShowComment = this.mode !== 'read' ? true : false;
-  }
+    /**
+     * Display comment field if in UPDATE or CREATE mode
+     */
+    updateCommentVisibility(): void {
+        this.shouldShowComment = this.mode !== 'read' ? true : false;
+    }
 
-  /**
-   * Toggles visibility of the comment field regardless of the mode
-   */
-  toggleCommentVisibility(): void {
-    this.shouldShowComment = !this.shouldShowComment;
-  }
+    /**
+     * Toggles visibility of the comment field regardless of the mode
+     */
+    toggleCommentVisibility(): void {
+        this.shouldShowComment = !this.shouldShowComment;
+    }
 
-  /**
-   * Returns a value that is to be created.
-   * Returns false if invalid.
-   */
-  abstract getNewValue(): CreateValue | false;
+    /**
+     * Returns a value that is to be created.
+     * Returns false if invalid.
+     */
+    abstract getNewValue(): CreateValue | false;
 
-  /**
-   * Returns a value that is to be updated.
-   * Returns false if invalid.
-   */
-  abstract getUpdatedValue(): UpdateValue | false;
+    /**
+     * Returns a value that is to be updated.
+     * Returns false if invalid.
+     */
+    abstract getUpdatedValue(): UpdateValue | false;
 
 
 }
--- projects/knora-ui/src/lib/viewer/values/interval-value/interval-value.component.ts
+++ projects/knora-ui/src/lib/viewer/values/interval-value/interval-value.component.ts
@@ -1,138 +1,139 @@
 import {Component, Inject, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild, NgZone} from '@angular/core';
 import {BaseValueComponent} from '../base-value.component';
 import {CreateIntervalValue, ReadIntervalValue, UpdateIntervalValue} from '@knora/api';
-import {AbstractControl, FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, ValidatorFn} from '@angular/forms';
+import {
+    AbstractControl,
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    FormGroupDirective,
+    NgForm,
+    ValidatorFn
+} from '@angular/forms';
 import {Subscription} from 'rxjs';
 import {Interval, IntervalInputComponent} from './interval-input/interval-input.component';
-import { ErrorStateMatcher } from '@angular/material/core';
-import { CdkTextareaAutosize } from '@angular/cdk/text-field';
-import { take } from 'rxjs/operators';
+import {ErrorStateMatcher} from '@angular/material/core';
+import {CdkTextareaAutosize} from '@angular/cdk/text-field';
+import {take} from 'rxjs/operators';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class IntervalErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
+    isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+        const isSubmitted = form && form.submitted;
+        return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+    }
 }
 
 @Component({
-  selector: 'kui-interval-value',
-  templateUrl: './interval-value.component.html',
-  styleUrls: ['./interval-value.component.scss']
+    selector: 'kui-interval-value',
+    templateUrl: './interval-value.component.html',
+    styleUrls: ['./interval-value.component.scss']
 })
 export class IntervalValueComponent extends BaseValueComponent implements OnInit, OnChanges, OnDestroy {
 
-  @ViewChild('intervalInput') intervalInputComponent: IntervalInputComponent;
-  @ViewChild('autosize') autosize: CdkTextareaAutosize;
+    @ViewChild('intervalInput') intervalInputComponent: IntervalInputComponent;
+    @ViewChild('autosize') autosize: CdkTextareaAutosize;
 
-  @Input() displayValue?: ReadIntervalValue;
+    @Input() displayValue?: ReadIntervalValue;
 
-  valueFormControl: FormControl;
-  commentFormControl: FormControl;
+    valueFormControl: FormControl;
+    commentFormControl: FormControl;
 
-  form: FormGroup;
+    form: FormGroup;
 
-  valueChangesSubscription: Subscription;
+    valueChangesSubscription: Subscription;
 
-  customValidators = [];
+    customValidators = [];
 
-  matcher = new IntervalErrorStateMatcher();
+    matcher = new IntervalErrorStateMatcher();
 
-  constructor(@Inject(FormBuilder) private fb: FormBuilder, private _ngZone: NgZone) {
-    super();
-  }
-
-  standardValidatorFunc: (val: any, comment: string, commentCtrl: FormControl) => ValidatorFn
-    = (initValue: any, initComment: string, commentFormControl: FormControl): ValidatorFn => {
-    return (control: AbstractControl): { [key: string]: any } | null => {
-
-      const invalid = (control.value !== null && initValue.start === control.value.start && initValue.end === control.value.end)
-        && (initComment === commentFormControl.value || (initComment === null && commentFormControl.value === ''));
-
-      return invalid ? {valueNotChanged: {value: control.value}} : null;
-    };
-  };
-
-  getInitValue(): Interval | null {
-    if (this.displayValue !== undefined) {
-      return new Interval(this.displayValue.start, this.displayValue.end);
-    } else {
-      return null;
-    }
-  }
-
-  ngOnInit() {
-    // initialize form control elements
-    this.valueFormControl = new FormControl(null);
-
-    this.commentFormControl = new FormControl(null);
-
-    // subscribe to any change on the comment and recheck validity
-    this.valueChangesSubscription = this.commentFormControl.valueChanges.subscribe(
-      data => {
-        this.valueFormControl.updateValueAndValidity();
-      }
-    );
-
-    this.form = this.fb.group({
-      intervalValue: this.valueFormControl,
-      comment: this.commentFormControl
-    });
-
-    this.resetFormControl();
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    this.resetFormControl();
-  }
-
-  // unsubscribe when the object is destroyed to prevent memory leaks
-  ngOnDestroy(): void {
-    this.unsubscribeFromValueChanges();
-  }
-
-  getNewValue(): CreateIntervalValue | false {
-    if (this.mode !== 'create' || !this.form.valid) {
-      return false;
+    constructor(@Inject(FormBuilder) private fb: FormBuilder, private _ngZone: NgZone) {
+        super();
     }
 
-    const newIntervalValue = new CreateIntervalValue();
-
-    newIntervalValue.start = this.valueFormControl.value.start;
-    newIntervalValue.end = this.valueFormControl.value.end;
-
-    if (this.commentFormControl.value !== null && this.commentFormControl.value !== '') {
-      newIntervalValue.valueHasComment = this.commentFormControl.value;
+    standardValueComparisonFunc(initValue: Interval, curValue: Interval | null): boolean {
+        return curValue !== null && initValue.start === curValue.start && initValue.end === curValue.end;
     }
 
-    return newIntervalValue;
-  }
-
-  getUpdatedValue(): UpdateIntervalValue | false {
-    if (this.mode !== 'update' || !this.form.valid) {
-      return false;
+    getInitValue(): Interval | null {
+        if (this.displayValue !== undefined) {
+            return new Interval(this.displayValue.start, this.displayValue.end);
+        } else {
+            return null;
+        }
     }
 
-    const updatedIntervalValue = new UpdateIntervalValue();
+    ngOnInit() {
+        // initialize form control elements
+        this.valueFormControl = new FormControl(null);
 
-    updatedIntervalValue.id = this.displayValue.id;
+        this.commentFormControl = new FormControl(null);
 
-    updatedIntervalValue.start = this.valueFormControl.value.start;
-    updatedIntervalValue.end = this.valueFormControl.value.end;
+        // subscribe to any change on the comment and recheck validity
+        this.valueChangesSubscription = this.commentFormControl.valueChanges.subscribe(
+            data => {
+                this.valueFormControl.updateValueAndValidity();
+            }
+        );
 
-    // add the submitted comment to updatedIntervalValue only if user has added a comment
-    if (this.commentFormControl.value !== null && this.commentFormControl.value !== '') {
-      updatedIntervalValue.valueHasComment = this.commentFormControl.value;
+        this.form = this.fb.group({
+            intervalValue: this.valueFormControl,
+            comment: this.commentFormControl
+        });
+
+        this.resetFormControl();
     }
 
-    return updatedIntervalValue;
-  }
+    ngOnChanges(changes: SimpleChanges): void {
+        this.resetFormControl();
+    }
 
-  triggerResize() {
-    // Wait for changes to be applied, then trigger textarea resize.
-    this._ngZone.onStable.pipe(take(1))
-        .subscribe(() => this.autosize.resizeToFitContent(true));
-  }
+    // unsubscribe when the object is destroyed to prevent memory leaks
+    ngOnDestroy(): void {
+        this.unsubscribeFromValueChanges();
+    }
+
+    getNewValue(): CreateIntervalValue | false {
+        if (this.mode !== 'create' || !this.form.valid) {
+            return false;
+        }
+
+        const newIntervalValue = new CreateIntervalValue();
+
+        newIntervalValue.start = this.valueFormControl.value.start;
+        newIntervalValue.end = this.valueFormControl.value.end;
+
+        if (this.commentFormControl.value !== null && this.commentFormControl.value !== '') {
+            newIntervalValue.valueHasComment = this.commentFormControl.value;
+        }
+
+        return newIntervalValue;
+    }
+
+    getUpdatedValue(): UpdateIntervalValue | false {
+        if (this.mode !== 'update' || !this.form.valid) {
+            return false;
+        }
+
+        const updatedIntervalValue = new UpdateIntervalValue();
+
+        updatedIntervalValue.id = this.displayValue.id;
+
+        updatedIntervalValue.start = this.valueFormControl.value.start;
+        updatedIntervalValue.end = this.valueFormControl.value.end;
+
+        // add the submitted comment to updatedIntervalValue only if user has added a comment
+        if (this.commentFormControl.value !== null && this.commentFormControl.value !== '') {
+            updatedIntervalValue.valueHasComment = this.commentFormControl.value;
+        }
+
+        return updatedIntervalValue;
+    }
+
+    triggerResize() {
+        // Wait for changes to be applied, then trigger textarea resize.
+        this._ngZone.onStable.pipe(take(1))
+            .subscribe(() => this.autosize.resizeToFitContent(true));
+    }
 
 }
