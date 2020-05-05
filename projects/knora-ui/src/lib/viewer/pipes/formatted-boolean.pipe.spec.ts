24bcf3257dfcf4794d3811ab918187ff5c913679
mdelez <60604010+mdelez@users.noreply.github.com>
2020-05-05 09:59:12 +0200

    test (pipe): add test for convertedBoolean pipe

--- projects/knora-ui/src/lib/viewer/pipes/formatted-boolean.pipe.spec.ts
+++ projects/knora-ui/src/lib/viewer/pipes/formatted-boolean.pipe.spec.ts
@@ -5,4 +5,13 @@ describe('FormattedBooleanPipe', () => {
     const pipe = new FormattedBooleanPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should return a string of false', () => {
+    const pipe = new FormattedBooleanPipe();
+    const myBoolean: boolean = false;
+
+    const convertedBoolean = pipe.transform(myBoolean);
+
+    expect(convertedBoolean).toEqual('false');
+});
 });
