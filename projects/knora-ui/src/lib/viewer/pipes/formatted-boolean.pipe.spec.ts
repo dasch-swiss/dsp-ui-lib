3630f43d56c95f57b0875ce48ee1cb9874c75013
mdelez <60604010+mdelez@users.noreply.github.com>
2020-05-05 14:45:10 +0200

    refactor (pipe): small changes

--- projects/knora-ui/src/lib/viewer/pipes/formatted-boolean.pipe.spec.ts
+++ projects/knora-ui/src/lib/viewer/pipes/formatted-boolean.pipe.spec.ts
@@ -1,17 +1,17 @@
 import { FormattedBooleanPipe } from './formatted-boolean.pipe';
 
 describe('FormattedBooleanPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FormattedBooleanPipe();
-    expect(pipe).toBeTruthy();
-  });
+    it('create an instance', () => {
+        const pipe = new FormattedBooleanPipe();
+        expect(pipe).toBeTruthy();
+    });
 
-  it('should return a string of false', () => {
-    const pipe = new FormattedBooleanPipe();
-    const myBoolean: boolean = false;
+    it('should return a string of false', () => {
+        const pipe = new FormattedBooleanPipe();
+        const myBoolean = false;
 
-    const convertedBoolean = pipe.transform(myBoolean);
+        const convertedBoolean = pipe.transform(myBoolean);
 
-    expect(convertedBoolean).toEqual('false');
-});
+        expect(convertedBoolean).toEqual('false');
+    });
 });
