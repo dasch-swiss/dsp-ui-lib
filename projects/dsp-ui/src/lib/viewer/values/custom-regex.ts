b072e6a8390d2ef76547774f6258396ef5710ad4
Sepideh Alassi <sepideh.alassi@gmail.com>
2020-05-13 17:07:36 +0200

    fix (ErrorHandling) regex pattern is fixed to avoid invalid decimals: -  multiple dots - scientific notations with exponents, i.e. "e" such as "-2.e32"

--- projects/dsp-ui/src/lib/viewer/values/custom-regex.ts
+++ projects/dsp-ui/src/lib/viewer/values/custom-regex.ts
@@ -5,7 +5,7 @@ export class CustomRegex {
 
     public static readonly INT_REGEX = /^-?\d+$/;
 
-    public static readonly DECIMAL_REGEX = /^(\d*\.)?\d+$/;
+    public static readonly DECIMAL_REGEX = /^[-+]?[0-9]*\.?[0-9]*$/;
 
     public static readonly URI_REGEX = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
 
