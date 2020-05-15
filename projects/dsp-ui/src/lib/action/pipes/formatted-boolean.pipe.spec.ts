be4ce2c069ee5e373d2f6e1b97c2f71a926060ff
mdelez <60604010+mdelez@users.noreply.github.com>
2020-05-15 17:52:00 +0200

    test (FormattedBooleanPipe): adding more unit tests

--- projects/dsp-ui/src/lib/action/pipes/formatted-boolean.pipe.spec.ts
+++ projects/dsp-ui/src/lib/action/pipes/formatted-boolean.pipe.spec.ts
@@ -1,6 +1,6 @@
 import { FormattedBooleanPipe } from './formatted-boolean.pipe';
 
-describe('FormattedBooleanPipe', () => {
+fdescribe('FormattedBooleanPipe', () => {
     let pipe: FormattedBooleanPipe;
 
     beforeEach(() => {
@@ -11,13 +11,35 @@ describe('FormattedBooleanPipe', () => {
         expect(pipe).toBeTruthy();
     });
 
-    it('should return "false"', () => {
-        const myBoolean = false;
+    it('should return "true" (no format specified)', () => {
+        const myBoolean = true;
 
         const convertedBoolean = pipe.transform(myBoolean);
+
+        expect(convertedBoolean).toEqual('true');
+    });
+
+    it('should return "false" (format specified)', () => {
+        const myBoolean = false;
+
+        const convertedBoolean = pipe.transform(myBoolean, 'true-false');
 
         expect(convertedBoolean).toEqual('false');
     });
 
+    it('should return "yes"', () => {
+        const myBoolean = true;
 
+        const convertedBoolean = pipe.transform(myBoolean, 'yes-no');
+
+        expect(convertedBoolean).toEqual('yes');
+    });
+
+    it('should return "off"', () => {
+        const myBoolean = false;
+
+        const convertedBoolean = pipe.transform(myBoolean, 'on-off');
+
+        expect(convertedBoolean).toEqual('off');
+    });
 });
