090f7e9dd75ecd9da00035648b14fcfc09571c8d
mdelez <60604010+mdelez@users.noreply.github.com>
2020-05-28 14:18:48 +0200

    activate all tests....

--- projects/dsp-ui/src/lib/action/directives/admin-image.directive.spec.ts
+++ projects/dsp-ui/src/lib/action/directives/admin-image.directive.spec.ts
@@ -14,7 +14,7 @@ class TestAdminImageComponent {
     constructor() { }
 }
 
-fdescribe('Directive: AdminImageDirective', () => {
+describe('Directive: AdminImageDirective', () => {
     let component: TestAdminImageComponent;
     let fixture: ComponentFixture<TestAdminImageComponent>;
     let imageEl: DebugElement;
