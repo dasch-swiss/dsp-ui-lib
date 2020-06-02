50edf5f7186853e416e1ca80edf2e8d2be313b81
Tobias Schweizer <t.schweizer@unibas.ch>
2020-06-02 09:44:24 +0200

    tests (e2e): set up basic e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,32 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, by, logging } from 'protractor';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+    let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+    beforeEach(() => {
+        page = new AppPage();
+    });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('dsp-ui-lib app is running!');
-  });
+    it('should display welcome message', async () => {
+        page.navigateTo('modify');
+        expect(page.getTitleText()).toEqual('dsp-ui-lib app is running!');
+    });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-  });
+    it('should display an integer value', async () => {
+        page.navigateTo('modify');
+        const ele = await page.getComponentBySelector('dsp-int-value').getWebElement();
+
+        const intValEle = await ele.findElement(by.css('.rm-value'));
+        expect(await intValEle.getText()).toEqual('1');
+
+    });
+
+    afterEach(async () => {
+        // Assert that there are no errors emitted from the browser
+        const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+        expect(logs).not.toContain(jasmine.objectContaining({
+            level: logging.Level.SEVERE,
+        } as logging.Entry));
+    });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,15 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+  navigateTo(segment: string) {
+    return browser.get(browser.baseUrl + segment) as Promise<any>;
   }
 
   getTitleText() {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return element(by.css('app-root span')).getText() as Promise<string>;
+  }
+
+  getComponentBySelector(selector: string): ElementFinder {
+      return element(by.css('app-root ' + selector));
   }
 }
