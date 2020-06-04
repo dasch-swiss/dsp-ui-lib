8cd1520bb84f8b8ea83ea7839222c20705569c8b
Tobias Schweizer <t.schweizer@unibas.ch>
2020-06-04 10:17:30 +0200

    test (test inputs): experiment with default values for inputs

--- src/app/development/input-test/input-test.component.spec.ts
+++ src/app/development/input-test/input-test.component.spec.ts
@@ -19,29 +19,83 @@ class TestHostComponent1 implements OnInit {
     }
 }
 
+/**
+ * Test host component to simulate parent component.
+ */
+@Component({
+    template: `
+        <app-input-test #testComp [mySecondInput]="'dudu'"></app-input-test>`
+})
+class TestHostComponent2 implements OnInit {
+
+    @ViewChild('testComp') testComponent: InputTestComponent;
+
+    ngOnInit() {
+
+    }
+}
+
+/**
+ * Test host component to simulate parent component.
+ */
+@Component({
+    template: `
+        <app-input-test #testComp [myThirdInput]="'aha'"></app-input-test>`
+})
+class TestHostComponent3 implements OnInit {
+
+    @ViewChild('testComp') testComponent: InputTestComponent;
+
+    ngOnInit() {
+
+    }
+}
+
 describe('InputTestComponent', () => {
-    let component: TestHostComponent1;
-    let fixture: ComponentFixture<TestHostComponent1>;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [InputTestComponent, TestHostComponent1]
+            declarations: [InputTestComponent, TestHostComponent1, TestHostComponent2, TestHostComponent3]
         })
             .compileComponents();
     }));
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(TestHostComponent1);
-        component = fixture.componentInstance;
+    it('should create InputTestComponent using TestHostComponent1', () => {
+        const fixture = TestBed.createComponent(TestHostComponent1);
+        const component = fixture.componentInstance;
         fixture.detectChanges();
-    });
 
-    it('should create', () => {
         expect(component).toBeTruthy();
 
         expect(component.testComponent).toBeTruthy();
         expect(component.testComponent.myFirstInput).toEqual('dada');
         expect(component.testComponent.mySecondInput).toEqual('hi');
         expect(component.testComponent.myThirdInput).toEqual(undefined);
+    });
+
+    it('should create InputTestComponent using TestHostComponent2', () => {
+        const fixture = TestBed.createComponent(TestHostComponent2);
+        const component = fixture.componentInstance;
+        fixture.detectChanges();
+
+        expect(component).toBeTruthy();
+
+        expect(component.testComponent).toBeTruthy();
+        expect(component.testComponent.myFirstInput).toEqual('oho');
+        expect(component.testComponent.mySecondInput).toEqual('dudu');
+        expect(component.testComponent.myThirdInput).toEqual(undefined);
+    });
+
+    it('should create InputTestComponent using TestHostComponent3', () => {
+        const fixture = TestBed.createComponent(TestHostComponent3);
+        const component = fixture.componentInstance;
+        fixture.detectChanges();
+
+        expect(component).toBeTruthy();
+
+        expect(component.testComponent).toBeTruthy();
+        expect(component.testComponent.myFirstInput).toEqual('oho');
+        expect(component.testComponent.mySecondInput).toEqual('hi');
+        expect(component.testComponent.myThirdInput).toEqual('aha');
     });
 });
