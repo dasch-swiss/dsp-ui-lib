d2af83576ea5b6c319517ea37823f07db6c8b661
flaurens <flavie.laurens@unibas.ch>
2020-06-10 16:57:42 +0200

    feat(search): Search-panel component creation and setup

--- projects/dsp-ui/src/lib/search/index.ts
+++ projects/dsp-ui/src/lib/search/index.ts
@@ -1,1 +1,3 @@
 export * from './search.module';
+
+export * from './search-panel/search-panel.component';
--- projects/dsp-ui/src/lib/search/search.module.ts
+++ projects/dsp-ui/src/lib/search/search.module.ts
@@ -1,10 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+
+import { SearchPanelComponent } from './search-panel/search-panel.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [
+        SearchPanelComponent
+    ],
+    imports: [
+        CommonModule,
+        MatIconModule,
+        MatMenuModule
+    ],
+    exports: [
+        SearchPanelComponent
+    ]
 })
 export class DspSearchModule { }
