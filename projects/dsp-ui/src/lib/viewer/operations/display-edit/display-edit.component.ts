2ea88b6469baa5f5d4f3c4b61d478fa1c7454da5
mdelez <60604010+mdelez@users.noreply.github.com>
2020-06-11 14:32:45 +0200

    refactor (display-edit): first not-so-elegant way of fixing dev build errors

--- projects/dsp-ui/src/lib/viewer/operations/display-edit/display-edit.component.ts
+++ projects/dsp-ui/src/lib/viewer/operations/display-edit/display-edit.component.ts
@@ -3,10 +3,21 @@ import {
   Constants,
   KnoraApiConnection,
   PermissionUtil,
+  ReadBooleanValue,
+  ReadColorValue,
+  ReadDateValue,
+  ReadDecimalValue,
+  ReadGeonameValue,
+  ReadIntervalValue,
+  ReadIntValue,
+  ReadLinkValue,
+  ReadListValue,
   ReadResource,
   ReadTextValueAsHtml,
   ReadTextValueAsString,
   ReadTextValueAsXml,
+  ReadTimeValue,
+  ReadUriValue,
   ReadValue,
   UpdateResource,
   UpdateValue,
@@ -23,41 +34,55 @@ import { BaseValueComponent } from '../../values/base-value.component';
 })
 export class DisplayEditComponent implements OnInit {
 
-  @ViewChild('displayVal') displayValueComponent: BaseValueComponent;
+    @ViewChild('displayVal') displayValueComponent: BaseValueComponent;
 
-  @Input() displayValue: ReadValue;
+    @Input() displayValue: ReadValue;
 
-  @Input() parentResource: ReadResource;
+    @Input() parentResource: ReadResource;
 
-  @Input() configuration?: object;
+    @Input() configuration?: object;
 
-  constants = Constants;
+    constants = Constants;
 
-  mode: 'read' | 'update' | 'create' | 'search';
+    mode: 'read' | 'update' | 'create' | 'search';
 
-  canModify: boolean;
+    canModify: boolean;
 
-  editModeActive = false;
+    editModeActive = false;
 
-  shouldShowCommentToggle: boolean;
+    shouldShowCommentToggle: boolean;
 
-  // type of given displayValue
-  // or knora-api-js-lib class representing the value
-  valueTypeOrClass: string;
+    readTextValAsString: ReadTextValueAsString;
+    readTextValAsHtml: ReadTextValueAsHtml;
+    readIntVal: ReadIntValue;
+    readBooleanVal: ReadBooleanValue;
+    readUriVal: ReadUriValue;
+    readDecimalVal: ReadDecimalValue;
+    readColorVal: ReadColorValue;
+    readIntervalVal: ReadIntervalValue;
+    readTimeVal: ReadTimeValue;
+    readGeonameVal: ReadGeonameValue;
+    readLinkVal: ReadLinkValue;
+    readDateVal: ReadDateValue;
+    readListVal: ReadListValue;
 
-  // indicates if value can be edited
-  readOnlyValue: boolean;
+    // type of given displayValue
+    // or knora-api-js-lib class representing the value
+    valueTypeOrClass: string;
 
-  private readonly readTextValueAsString = 'ReadTextValueAsString';
+    // indicates if value can be edited
+    readOnlyValue: boolean;
 
-  private readonly readTextValueAsXml = 'ReadTextValueAsXml';
+    private readonly readTextValueAsString = 'ReadTextValueAsString';
 
-  private readonly readTextValueAsHtml = 'ReadTextValueAsHtml';
+    private readonly readTextValueAsXml = 'ReadTextValueAsXml';
 
-  constructor(@Inject(DspApiConnectionToken) private knoraApiConnection: KnoraApiConnection) {
-  }
+    private readonly readTextValueAsHtml = 'ReadTextValueAsHtml';
 
-  ngOnInit() {
+    constructor(@Inject(DspApiConnectionToken) private knoraApiConnection: KnoraApiConnection) {
+    }
+
+    ngOnInit() {
 
     this.mode = 'read';
 
@@ -69,12 +94,14 @@ export class DisplayEditComponent implements OnInit {
     // check if comment toggle button should be shown
     this.checkCommentToggleVisibility();
 
+    this.checkValueType();
+
     this.valueTypeOrClass = this.getValueTypeOrClass(this.displayValue);
 
     this.readOnlyValue = this.isReadOnly(this.valueTypeOrClass);
-  }
+    }
 
-  activateEditMode() {
+    activateEditMode() {
     this.editModeActive = true;
     this.mode = 'update';
 
@@ -83,100 +110,130 @@ export class DisplayEditComponent implements OnInit {
 
     // hide read mode comment when switching to edit mode
     this.displayValueComponent.shouldShowComment = false;
-  }
+    }
 
-  saveEditValue() {
+    saveEditValue() {
     this.editModeActive = false;
     const updatedVal = this.displayValueComponent.getUpdatedValue();
 
     if (updatedVal instanceof UpdateValue) {
 
-      const updateRes = new UpdateResource();
-      updateRes.id = this.parentResource.id;
-      updateRes.type = this.parentResource.type;
-      updateRes.property = this.displayValue.property;
-      updateRes.value = updatedVal;
-      this.knoraApiConnection.v2.values.updateValue(updateRes as UpdateResource<UpdateValue>).pipe(
+        const updateRes = new UpdateResource();
+        updateRes.id = this.parentResource.id;
+        updateRes.type = this.parentResource.type;
+        updateRes.property = this.displayValue.property;
+        updateRes.value = updatedVal;
+        this.knoraApiConnection.v2.values.updateValue(updateRes as UpdateResource<UpdateValue>).pipe(
         mergeMap((res: WriteValueResponse) => {
-          return this.knoraApiConnection.v2.values.getValue(this.parentResource.id, res.uuid);
+            return this.knoraApiConnection.v2.values.getValue(this.parentResource.id, res.uuid);
         })
-      ).subscribe(
+        ).subscribe(
         (res2: ReadResource) => {
-          this.displayValue = res2.getValues(this.displayValue.property)[0];
-          this.mode = 'read';
+            this.displayValue = res2.getValues(this.displayValue.property)[0];
+            this.mode = 'read';
 
-          // hide comment once back in read mode
-          this.displayValueComponent.updateCommentVisibility();
+            // hide comment once back in read mode
+            this.displayValueComponent.updateCommentVisibility();
 
-          // check if comment toggle button should be shown
-          this.checkCommentToggleVisibility();
+            // check if comment toggle button should be shown
+            this.checkCommentToggleVisibility();
         }
-      );
+        );
 
     } else {
-      console.error('invalid value');
+        console.error('invalid value');
     }
-  }
-
-  cancelEditValue() {
-    this.editModeActive = false;
-    this.mode = 'read';
-
-    // hide comment once back in read mode
-    this.displayValueComponent.updateCommentVisibility();
-
-    // check if comment toggle button should be shown
-    this.checkCommentToggleVisibility();
-  }
-
-  // shows or hides the comment
-  toggleComment() {
-    this.displayValueComponent.toggleCommentVisibility();
-  }
-
-  // only show the comment toggle button if user is in READ mode and a comment exists for the value
-  checkCommentToggleVisibility() {
-    this.shouldShowCommentToggle = (this.mode === 'read' && this.displayValue.valueHasComment !== '' && this.displayValue.valueHasComment !== undefined);
-  }
-
-  /**
-   * Given a value, determines the type or class representing it.
-   *
-   * For text values, this method determines the specific class in use.
-   * For all other types, the given type is returned.
-   *
-   * @param value the given value.
-   */
-  getValueTypeOrClass(value: ReadValue): string {
-
-    if (value.type === this.constants.TextValue) {
-      if (value instanceof ReadTextValueAsString) {
-        return this.readTextValueAsString;
-      } else if (value instanceof ReadTextValueAsXml) {
-        return this.readTextValueAsXml;
-      } else if (value instanceof ReadTextValueAsHtml) {
-        return this.readTextValueAsHtml;
-      } else {
-        throw new Error(`unknown TextValue class ${value}`);
-      }
-    } else {
-      return value.type;
     }
-  }
 
-  /**
-   * Equality checks with constants below are TEMPORARY until component is implemented.
-   * Used so that the CRUD buttons do not show if a property doesn't have a value component.
-   */
+    cancelEditValue() {
+        this.editModeActive = false;
+        this.mode = 'read';
 
-  /**
-   * Determines if the given value is readonly.
-   *
-   * @param valueTypeOrClass the type or class of the given value.
-   */
-  isReadOnly(valueTypeOrClass: string): boolean {
-    return valueTypeOrClass === this.readTextValueAsHtml ||
-           valueTypeOrClass === this.readTextValueAsXml  ||
-           valueTypeOrClass === this.constants.GeomValue;
-  }
+        // hide comment once back in read mode
+        this.displayValueComponent.updateCommentVisibility();
+
+        // check if comment toggle button should be shown
+        this.checkCommentToggleVisibility();
+    }
+
+    // shows or hides the comment
+    toggleComment() {
+        this.displayValueComponent.toggleCommentVisibility();
+    }
+
+    // only show the comment toggle button if user is in READ mode and a comment exists for the value
+    checkCommentToggleVisibility() {
+        this.shouldShowCommentToggle = (this.mode === 'read' && this.displayValue.valueHasComment !== '' && this.displayValue.valueHasComment !== undefined);
+    }
+
+    /**
+     * Given a value, determines the type or class representing it.
+     *
+     * For text values, this method determines the specific class in use.
+     * For all other types, the given type is returned.
+     *
+     * @param value the given value.
+     */
+    getValueTypeOrClass(value: ReadValue): string {
+
+        if (value.type === this.constants.TextValue) {
+            if (value instanceof ReadTextValueAsString) {
+                return this.readTextValueAsString;
+            } else if (value instanceof ReadTextValueAsXml) {
+                return this.readTextValueAsXml;
+            } else if (value instanceof ReadTextValueAsHtml) {
+                return this.readTextValueAsHtml;
+            } else {
+                throw new Error(`unknown TextValue class ${value}`);
+            }
+        } else {
+            return value.type;
+        }
+    }
+
+    checkValueType(): void {
+        if (this.displayValue instanceof ReadTextValueAsString) {
+            this.readTextValAsString = this.displayValue;
+        } else if (this.displayValue instanceof ReadTextValueAsHtml) {
+            this.readTextValAsHtml = this.displayValue;
+        } else if (this.displayValue instanceof ReadIntValue) {
+            this.readIntVal = this.displayValue;
+        } else if (this.displayValue instanceof ReadBooleanValue) {
+            this.readBooleanVal = this.displayValue;
+        } else if (this.displayValue instanceof ReadUriValue) {
+            this.readUriVal = this.displayValue;
+        } else if (this.displayValue instanceof ReadDecimalValue) {
+            this.readDecimalVal = this.displayValue;
+        } else if (this.displayValue instanceof ReadColorValue) {
+            this.readColorVal = this.displayValue;
+        } else if (this.displayValue instanceof ReadIntervalValue) {
+            this.readIntervalVal = this.displayValue;
+        } else if (this.displayValue instanceof ReadTimeValue) {
+            this.readTimeVal = this.displayValue;
+        } else if (this.displayValue instanceof ReadGeonameValue) {
+            this.readGeonameVal = this.displayValue;
+        } else if (this.displayValue instanceof ReadLinkValue) {
+            this.readLinkVal = this.displayValue;
+        } else if (this.displayValue instanceof ReadDateValue) {
+            this.readDateVal = this.displayValue;
+        } else if (this.displayValue instanceof ReadListValue) {
+            this.readListVal = this.displayValue;
+        }
+    }
+
+    /**
+     * Equality checks with constants below are TEMPORARY until component is implemented.
+     * Used so that the CRUD buttons do not show if a property doesn't have a value component.
+     */
+
+    /**
+     * Determines if the given value is readonly.
+     *
+     * @param valueTypeOrClass the type or class of the given value.
+     */
+    isReadOnly(valueTypeOrClass: string): boolean {
+        return valueTypeOrClass === this.readTextValueAsHtml ||
+                valueTypeOrClass === this.readTextValueAsXml  ||
+                valueTypeOrClass === this.constants.GeomValue;
+    }
 }
