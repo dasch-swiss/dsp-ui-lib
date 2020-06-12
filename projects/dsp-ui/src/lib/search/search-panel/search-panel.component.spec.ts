097ef8497f7d03c1127927ac50f88f8d2bca629d
flaurens <flavie.laurens@unibas.ch>
2020-06-12 14:33:44 +0200

    test(search-panel): Set up

--- projects/dsp-ui/src/lib/search/search-panel/search-panel.component.spec.ts
+++ projects/dsp-ui/src/lib/search/search-panel/search-panel.component.spec.ts
@@ -1,28 +1,60 @@
 import { OverlayModule } from '@angular/cdk/overlay';
+import { Component, ViewChild } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatMenuModule } from '@angular/material/menu';
 import { SearchPanelComponent } from './search-panel.component';
 
+/**
+ * Test host component to simulate parent component with a search panel.
+ */
+@Component({
+    template: `
+        <dsp-search-panel #searchPanelView
+            [route]="route"
+            [projectfilter]="projectfilter"
+            [expert]="expert"
+            [advanced]="advanced">
+        </dsp-search-panel>`
+})
+class TestHostComponent {
+
+    @ViewChild('searchPanelView') searchPanelComponent: SearchPanelComponent;
+
+    route = '/search';
+    projectfilter = true;
+    advanced = false;
+    expert = false;
+
+}
 
 describe('SearchPanelComponent', () => {
-  let component: SearchPanelComponent;
-  let fixture: ComponentFixture<SearchPanelComponent>;
+    let testHostComponent: TestHostComponent;
+    let testHostFixture: ComponentFixture<TestHostComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SearchPanelComponent ],
-      imports: [ OverlayModule, MatMenuModule ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                SearchPanelComponent,
+                TestHostComponent
+            ],
+            imports: [
+                OverlayModule,
+                MatMenuModule
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SearchPanelComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        testHostFixture = TestBed.createComponent(TestHostComponent);
+        testHostComponent = testHostFixture.componentInstance;
+        testHostFixture.detectChanges();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        expect(testHostComponent).toBeTruthy();
+    });
+
+    it('should create an instance', () => {
+        expect(testHostComponent.searchPanelComponent).toBeTruthy();
+    });
+
 });
