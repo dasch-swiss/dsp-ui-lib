2790e511d477a73287ed886b1e053ea1869bdc1f
Tobias Schweizer <t.schweizer@unibas.ch>
2020-06-12 08:29:03 +0200

    feature (search): add select ontology comp (raw)

---
+++ projects/dsp-ui/src/lib/search/advanced-search/select-ontology/select-ontology.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dsp-select-ontology',
+  templateUrl: './select-ontology.component.html',
+  styleUrls: ['./select-ontology.component.scss']
+})
+export class SelectOntologyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- projects/dsp-ui/src/lib/search/search.module.ts
+++ projects/dsp-ui/src/lib/search/search.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SelectOntologyComponent } from './advanced-search/select-ontology/select-ontology.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [SelectOntologyComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: []
 })
 export class DspSearchModule { }
