1c4a92aa4911826e58be375692d1538ec587ceeb
mdelez <60604010+mdelez@users.noreply.github.com>
2020-06-15 15:14:25 +0200

    tests (session service): attempting to write unit tests

--- projects/dsp-ui/src/lib/core/session.service.spec.ts
+++ projects/dsp-ui/src/lib/core/session.service.spec.ts
@@ -1,31 +1,78 @@
-import { TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
+import { KnoraApiConfig, KnoraApiConnection, UsersEndpointAdmin, ApiResponseData, UserResponse, ApiResponseError, User, ReadUser } from '@knora/api';
 import { DspApiConfigToken, DspApiConnectionToken } from './core.module';
-import { SessionService } from './session.service';
-import { KnoraApiConnection, KnoraApiConfig } from '@knora/api';
+import { SessionService, Session } from './session.service';
+import { of, Observable } from 'rxjs';
 
 describe('SessionService', () => {
 
     let service: SessionService;
 
-    beforeEach(() => {
+    const config = new KnoraApiConfig('http', 'localhost', 3333, undefined, undefined, true);
+    const knoraApiConnection = new KnoraApiConnection(config);
+
+    beforeEach(async(() => {
+
+        const adminSpyObj = {
+            admin: {
+                usersEndpoint: jasmine.createSpyObj('usersEndpoint', ['getUser'])
+            }
+        };
+
         TestBed.configureTestingModule({
             imports: [],
             providers: [
                 SessionService,
                 {
                     provide: DspApiConfigToken,
-                    useValue: KnoraApiConfig
+                    useValue: config
                 },
                 {
                     provide: DspApiConnectionToken,
-                    useValue: KnoraApiConnection
-                }
+                    useValue: knoraApiConnection
+                },
             ]
         });
         service = TestBed.inject(SessionService);
+    }));
+
+    // mock localStorage
+    beforeEach(() => {
+        let store = {};
+
+        spyOn(localStorage, 'getItem').and.callFake(
+            (key: string): string => {
+                return store[key] || null;
+            }
+        );
+        spyOn(localStorage, 'removeItem').and.callFake(
+            (key: string): void => {
+                delete store[key];
+            }
+        );
+        spyOn(localStorage, 'setItem').and.callFake(
+            (key: string, value: string): string => {
+                return (store[key] = value as any);
+            }
+        );
+        spyOn(localStorage, 'clear').and.callFake(() => {
+            store = {};
+        });
+
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should store a users jwt in local storage', () => {
+
+        service.setSession('jwtToken', 'root', 'username');
+
+        const ls = JSON.parse(localStorage.getItem('session')) as Session;
+
+        console.log(ls);
+
+        expect(ls.user.jwt).toEqual('jwtToken');
     });
 });
