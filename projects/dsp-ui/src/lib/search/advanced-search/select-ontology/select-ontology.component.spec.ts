22a1a7eda820fd9f4445ab5028c911ad2ad12f7f
Tobias Schweizer <t.schweizer@unibas.ch>
2020-06-15 10:12:11 +0200

    tests (search): add test to select ontology component

--- projects/dsp-ui/src/lib/search/advanced-search/select-ontology/select-ontology.component.spec.ts
+++ projects/dsp-ui/src/lib/search/advanced-search/select-ontology/select-ontology.component.spec.ts
@@ -7,6 +7,9 @@ import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { MatSelectHarness } from '@angular/material/select/testing';
 
 /**
  * Test host component to simulate parent component.
@@ -55,6 +58,8 @@ describe('SelectOntologyComponent', () => {
     let testHostComponent: TestHostDisplayValueComponent;
     let testHostFixture: ComponentFixture<TestHostDisplayValueComponent>;
 
+    let loader: HarnessLoader;
+
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [
@@ -73,11 +78,37 @@ describe('SelectOntologyComponent', () => {
     beforeEach(() => {
         testHostFixture = TestBed.createComponent(TestHostDisplayValueComponent);
         testHostComponent = testHostFixture.componentInstance;
+        loader = TestbedHarnessEnvironment.loader(testHostFixture);
+
         testHostFixture.detectChanges();
     });
 
     it('should create', () => {
         expect(testHostComponent).toBeTruthy();
         expect(testHostComponent.selectOntology).toBeTruthy();
+    });
+
+    it('should init the MatSelect and MatOptions correctly', async () => {
+
+        const select = await loader.getHarness(MatSelectHarness);
+        const initVal = await select.getValueText();
+
+        // placeholder
+        expect(initVal).toEqual('Select an Ontology');
+
+        await select.open();
+
+        const options = await select.getOptions();
+
+        expect(options.length).toEqual(2);
+
+        const option1 = await options[0].getText();
+
+        expect(option1).toEqual('anythingOnto');
+
+        const option2 = await options[1].getText();
+
+        expect(option2).toEqual('somethingOnto');
+
     });
 });
