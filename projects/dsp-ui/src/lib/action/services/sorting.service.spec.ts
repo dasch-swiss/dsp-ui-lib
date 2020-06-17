d1d7951af32fec1cbfc23e73998d651bb948ff8a
Tobias Schweizer <t.schweizer@unibas.ch>
2020-06-17 18:56:47 +0200

    test (action): make separate describe block

--- projects/dsp-ui/src/lib/action/services/sorting.service.spec.ts
+++ projects/dsp-ui/src/lib/action/services/sorting.service.spec.ts
@@ -4,136 +4,157 @@ import { SortingService } from './sorting.service';
 import { ReadProject } from '@dasch-swiss/dsp-js';
 
 describe('SortingService', () => {
-  let service: SortingService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-        providers: [
-            SortingService
-        ]
-    });
-
-    service = TestBed.inject(SortingService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-
-  describe('reverseArray', () => {
-
-    let data: string[];
+    let service: SortingService;
 
     beforeEach(() => {
-        data = ['Bernouilli', 'Euler', 'Goldbach', 'Hermann'];
+        TestBed.configureTestingModule({
+            providers: [
+                SortingService
+            ]
+        });
+
+        service = TestBed.inject(SortingService);
     });
 
-    it('should reverse an array', () => {
-        expect(service.reverseArray(data)).toEqual(['Hermann', 'Goldbach', 'Euler', 'Bernouilli']);
-      });
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 
-  describe('sortByAlphabetical', () => {
+    describe('reverseArray', () => {
 
-    let data:
-    {
-        firstname: string;
-        lastname: string;
-        creator: string;
-    }[];
+        let data: string[];
 
-    beforeEach(() => {
-        data = [
+        beforeEach(() => {
+            data = ['Bernouilli', 'Euler', 'Goldbach', 'Hermann'];
+        });
+
+        it('should reverse an array', () => {
+            expect(service.reverseArray(data)).toEqual(['Hermann', 'Goldbach', 'Euler', 'Bernouilli']);
+        });
+    });
+
+    describe('sortByAlphabetical', () => {
+
+        let data:
             {
-                firstname: 'Gaston',
-                lastname: 'Lagaffe',
-                creator: 'André Franquin'
-            },
-            {
-                firstname: 'Mickey',
-                lastname: 'Mouse',
-                creator: 'Walt Disney'
-            },
-            {
-                firstname: 'Gyro',
-                lastname: 'Gearloose',
-                creator: 'Carl Barks'
-            },
-            {
-                firstname: 'Charlie',
-                lastname: 'Brown',
-                creator: 'Charles M. Schulz'
-            }
-    ];
-    });
+                firstname: string;
+                lastname: string;
+                creator: string;
+            }[];
 
-    it('should return an array sorted by creator', () => {
-        const sorted = service.keySortByAlphabetical(data, 'creator');
-        expect(sorted).toEqual(
-            [
-                { firstname: 'Gaston', lastname: 'Lagaffe', creator: 'André Franquin' },
-                { firstname: 'Gyro', lastname: 'Gearloose', creator: 'Carl Barks' },
-                { firstname: 'Charlie', lastname: 'Brown', creator: 'Charles M. Schulz' },
-                { firstname: 'Mickey', lastname: 'Mouse', creator: 'Walt Disney' }
-            ]);
-    });
+        beforeEach(() => {
+            data = [
+                {
+                    firstname: 'Gaston',
+                    lastname: 'Lagaffe',
+                    creator: 'André Franquin'
+                },
+                {
+                    firstname: 'Mickey',
+                    lastname: 'Mouse',
+                    creator: 'Walt Disney'
+                },
+                {
+                    firstname: 'Gyro',
+                    lastname: 'Gearloose',
+                    creator: 'Carl Barks'
+                },
+                {
+                    firstname: 'Charlie',
+                    lastname: 'Brown',
+                    creator: 'Charles M. Schulz'
+                }
+            ];
+        });
 
-    it('should return an array sorted by firstname', () => {
-        const sorted = service.keySortByAlphabetical(data, 'firstname');
-        expect(sorted).toEqual(
-            [
-                { firstname: 'Charlie', lastname: 'Brown', creator: 'Charles M. Schulz' },
-                { firstname: 'Gaston', lastname: 'Lagaffe', creator: 'André Franquin' },
-                { firstname: 'Gyro', lastname: 'Gearloose', creator: 'Carl Barks' },
-                { firstname: 'Mickey', lastname: 'Mouse', creator: 'Walt Disney' }
-            ]);
-    });
+        it('should return an array sorted by creator', () => {
+            const sorted = service.keySortByAlphabetical(data, 'creator');
+            expect(sorted).toEqual(
+                [
+                    {firstname: 'Gaston', lastname: 'Lagaffe', creator: 'André Franquin'},
+                    {firstname: 'Gyro', lastname: 'Gearloose', creator: 'Carl Barks'},
+                    {firstname: 'Charlie', lastname: 'Brown', creator: 'Charles M. Schulz'},
+                    {firstname: 'Mickey', lastname: 'Mouse', creator: 'Walt Disney'}
+                ]);
+        });
 
-    it('should return an array sorted by lastname', () => {
-        const sorted = service.keySortByAlphabetical(data, 'lastname');
-        expect(sorted).toEqual(
-            [
-                { firstname: 'Charlie', lastname: 'Brown', creator: 'Charles M. Schulz' },
-                { firstname: 'Gyro', lastname: 'Gearloose', creator: 'Carl Barks' },
-                { firstname: 'Gaston', lastname: 'Lagaffe', creator: 'André Franquin' },
-                { firstname: 'Mickey', lastname: 'Mouse', creator: 'Walt Disney' }
-            ]);
-    });
+        it('should return an array sorted by firstname', () => {
+            const sorted = service.keySortByAlphabetical(data, 'firstname');
+            expect(sorted).toEqual(
+                [
+                    {firstname: 'Charlie', lastname: 'Brown', creator: 'Charles M. Schulz'},
+                    {firstname: 'Gaston', lastname: 'Lagaffe', creator: 'André Franquin'},
+                    {firstname: 'Gyro', lastname: 'Gearloose', creator: 'Carl Barks'},
+                    {firstname: 'Mickey', lastname: 'Mouse', creator: 'Walt Disney'}
+                ]);
+        });
 
-    it('should return an array sorted by lastname reversed', () => {
-        const sorted = service.keySortByAlphabetical(data, 'lastname', true);
-        expect(sorted).toEqual(
-            [
-                { firstname: 'Mickey', lastname: 'Mouse', creator: 'Walt Disney' },
-                { firstname: 'Gaston', lastname: 'Lagaffe', creator: 'André Franquin' },
-                { firstname: 'Gyro', lastname: 'Gearloose', creator: 'Carl Barks' },
-                { firstname: 'Charlie', lastname: 'Brown', creator: 'Charles M. Schulz' }
-            ]);
-    });
+        it('should return an array sorted by lastname', () => {
+            const sorted = service.keySortByAlphabetical(data, 'lastname');
+            expect(sorted).toEqual(
+                [
+                    {firstname: 'Charlie', lastname: 'Brown', creator: 'Charles M. Schulz'},
+                    {firstname: 'Gyro', lastname: 'Gearloose', creator: 'Carl Barks'},
+                    {firstname: 'Gaston', lastname: 'Lagaffe', creator: 'André Franquin'},
+                    {firstname: 'Mickey', lastname: 'Mouse', creator: 'Walt Disney'}
+                ]);
+        });
 
-    it('should sort an array of ReadProject by "longname"', () => {
-
-        const project1 = new ReadProject();
-        project1.id = '1';
-        project1.longname = 'a';
-
-        const project2 = new ReadProject();
-        project2.id = '2';
-        project1.longname = 'b';
-
-        const project3 = new ReadProject();
-        project3.id = '3';
-        project1.longname = 'c';
-
-        const projects = [project2, project3, project1];
-
-        const sorted = service.keySortByAlphabetical(projects, 'longname');
-
-        expect(sorted).toEqual(
-            [project1, project2, project3]
-        );
+        it('should return an array sorted by lastname reversed', () => {
+            const sorted = service.keySortByAlphabetical(data, 'lastname', true);
+            expect(sorted).toEqual(
+                [
+                    {firstname: 'Mickey', lastname: 'Mouse', creator: 'Walt Disney'},
+                    {firstname: 'Gaston', lastname: 'Lagaffe', creator: 'André Franquin'},
+                    {firstname: 'Gyro', lastname: 'Gearloose', creator: 'Carl Barks'},
+                    {firstname: 'Charlie', lastname: 'Brown', creator: 'Charles M. Schulz'}
+                ]);
+        });
 
     });
-  });
+
+    describe('Sort an array of ReadProject', () => {
+        let project1: ReadProject;
+        let project2: ReadProject;
+        let project3: ReadProject;
+        let projects: ReadProject[];
+
+        beforeEach(() => {
+            project1 = new ReadProject();
+            project1.id = '1';
+            project1.longname = 'a';
+
+            project2 = new ReadProject();
+            project2.id = '2';
+            project1.longname = 'b';
+
+            project3 = new ReadProject();
+            project3.id = '3';
+            project1.longname = 'c';
+
+            projects = [project2, project3, project1];
+        });
+
+        it('should sort an array of ReadProject by "longname"', () => {
+
+            const sorted = service.keySortByAlphabetical(projects, 'longname');
+
+            expect(sorted).toEqual(
+                [project1, project2, project3]
+            );
+
+        });
+
+        it('should sort an array of ReadProject by "longname" reversed', () => {
+
+            const sorted = service.keySortByAlphabetical(projects, 'longname', true);
+
+            expect(sorted).toEqual(
+                [project3, project2, project1]
+            );
+
+        });
+
+    });
 
 });
