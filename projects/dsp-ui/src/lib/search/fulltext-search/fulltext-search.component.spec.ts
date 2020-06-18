2e2d662f6fdabdf3b3c5b974ed01463b7b49eb3d
flaurens <flavie.laurens@unibas.ch>
2020-06-18 16:08:59 +0200

    test(fulltext): Mock parent component - wip

--- projects/dsp-ui/src/lib/search/fulltext-search/fulltext-search.component.spec.ts
+++ projects/dsp-ui/src/lib/search/fulltext-search/fulltext-search.component.spec.ts
@@ -1,41 +1,89 @@
 import { OverlayModule } from '@angular/cdk/overlay';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MockResource } from '@dasch-swiss/dsp-js';
 import { DspApiConnectionToken } from '../../core';
 import { FulltextSearchComponent } from './fulltext-search.component';
 
+/**
+ * Test host component to simulate parent component.
+ */
+@Component({
+    selector: `lib-host-component`,
+    template: `
+        <dsp-fulltext-search #fulltextSearch class="kui-fulltext-search"[route]="route"[projectfilter]="projectfilter"
+        [filterbyproject]="filterbyproject" >
+            </dsp-fulltext-search>
+    `
+})
+class TestHostFulltextSearchComponent implements OnInit {
+
+    @ViewChild('fulltextSearch') fulltextSearchComponent: FulltextSearchComponent;
+
+    route: string = '/search';
+    projectfilter: boolean = false;
+    filterbyproject?: string;
+
+    ngOnInit() {
+        MockResource.getTestthing().subscribe(res => {
+            console.log('res', res);
+            this.filterbyproject = res.attachedToProject;
+        });
+    }
+
+}
+
 
 describe('FulltextSearchComponent', () => {
-  let component: FulltextSearchComponent;
-  let fixture: ComponentFixture<FulltextSearchComponent>;
+    let testHostComponent: TestHostFulltextSearchComponent;
+    let testHostFixture: ComponentFixture<TestHostFulltextSearchComponent>;
 
-  const spyObj = {
-    v2: {
-      res: jasmine.createSpyObj('projectsEndpoint', ['getProjects'])
-    }
-  };
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FulltextSearchComponent ],
-      imports: [OverlayModule, RouterTestingModule],
-      providers: [
-        {
-          provide: DspApiConnectionToken,
-          useValue: spyObj
+    const dspConnSpy = {
+        admin: {
+            projects: jasmine.createSpyObj('projectsEndpoint', ['getProjects']),
+            projByIri: jasmine.createSpyObj('projectsEndpoint', ['getProjectByIri'])
         }
-      ]
-    })
-    .compileComponents();
-  }));
+    };
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FulltextSearchComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                FulltextSearchComponent,
+                TestHostFulltextSearchComponent
+            ],
+            imports: [
+                OverlayModule,
+                RouterTestingModule
+            ],
+            providers: [
+                {
+                    provide: DspApiConnectionToken,
+                    useValue: dspConnSpy
+                }
+            ]
+        })
+            .compileComponents();
+    }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        /* const dspConnSpy = TestBed.inject(DspApiConnectionToken);
+
+        (dspConnSpy.admin.projectsEndpoint as jasmine.SpyObj<ProjectsEndpointAdmin>).getProjectByIri.and.callFake(
+            (projIri: string) => {
+
+                // create fake data and return project
+
+            }
+        ); */
+
+        testHostFixture = TestBed.createComponent(TestHostFulltextSearchComponent);
+        testHostComponent = testHostFixture.componentInstance;
+        testHostFixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(testHostComponent).toBeTruthy();
+        expect(testHostComponent.fulltextSearchComponent).toBeTruthy();
+    });
 });
