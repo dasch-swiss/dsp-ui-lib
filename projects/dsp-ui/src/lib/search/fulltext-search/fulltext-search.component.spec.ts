80dee5a2928a0da3e706e84032cfe8ca935dfc3f
flaurens <flavie.laurens@unibas.ch>
2020-06-19 16:38:00 +0200

    test(fulltext-search): Add tests

--- projects/dsp-ui/src/lib/search/fulltext-search/fulltext-search.component.spec.ts
+++ projects/dsp-ui/src/lib/search/fulltext-search/fulltext-search.component.spec.ts
@@ -1,9 +1,17 @@
 import { OverlayModule } from '@angular/cdk/overlay';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { By } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
-import { MockProjects, ProjectsEndpointAdmin, ReadProject } from '@dasch-swiss/dsp-js';
+import { MockProjects, ProjectsEndpointAdmin } from '@dasch-swiss/dsp-js';
 import { of } from 'rxjs/internal/observable/of';
 import { SortingService } from '../../action/services/sorting.service';
 import { DspApiConnectionToken } from '../../core';
@@ -15,9 +23,12 @@ import { FulltextSearchComponent } from './fulltext-search.component';
 @Component({
     selector: `dsp-host-component`,
     template: `
-        <dsp-fulltext-search #fulltextSearch class="kui-fulltext-search"[route]="route"[projectfilter]="projectfilter"
-        [filterbyproject]="filterbyproject" >
-            </dsp-fulltext-search>
+        <dsp-fulltext-search #fulltextSearch
+            [route]="route"
+            [projectfilter]="projectfilter"
+            [filterbyproject]="filterbyproject"
+            class="kui-fulltext-search">
+        </dsp-fulltext-search>
     `
 })
 class TestHostFulltextSearchComponent implements OnInit {
@@ -39,6 +50,8 @@ class TestHostFulltextSearchComponent implements OnInit {
 describe('FulltextSearchComponent', () => {
     let testHostComponent: TestHostFulltextSearchComponent;
     let testHostFixture: ComponentFixture<TestHostFulltextSearchComponent>;
+    let fulltextSearchComponentDe;
+    let hostCompDe;
 
     const dspConnSpy = {
         admin: {
@@ -55,7 +68,14 @@ describe('FulltextSearchComponent', () => {
             imports: [
                 OverlayModule,
                 RouterTestingModule,
-                MatMenuModule
+                FormsModule,
+                BrowserAnimationsModule,
+                MatMenuModule,
+                MatInputModule,
+                MatTooltipModule,
+                MatIconModule,
+                MatDividerModule,
+                MatListModule
             ],
             providers: [
                 {
@@ -81,6 +101,9 @@ describe('FulltextSearchComponent', () => {
         testHostFixture = TestBed.createComponent(TestHostFulltextSearchComponent);
         testHostComponent = testHostFixture.componentInstance;
         testHostFixture.detectChanges();
+
+        hostCompDe = testHostFixture.debugElement;
+        fulltextSearchComponentDe = hostCompDe.query(By.directive(FulltextSearchComponent));
     });
 
     it('should create', () => {
@@ -96,6 +119,61 @@ describe('FulltextSearchComponent', () => {
         expect(testHostComponent.fulltextSearch.projectfilter).toEqual(true);
 
         expect(projSpy.admin.projectsEndpoint.getProjects).toHaveBeenCalledTimes(2);
+
+    });
+
+    // TODO: perform a search
+    // TODO: reset search
+    // TODO: do prev search
+    // TODO: reset prev search
+
+    it('should get a menu panel with the list of projects', () => {
+
+        const projButtonDe = fulltextSearchComponentDe.query(By.css('button.kui-project-filter-button'));
+        const projButtonNe = projButtonDe.nativeElement;
+
+        expect(projButtonNe).toBeDefined();
+
+        const projBtnLabelDe = projButtonDe.query(By.css('button > p.label'));
+        const projBtnLabelNe = projBtnLabelDe.nativeElement;
+        console.log('projBtnLabelNe.innerHTML', projBtnLabelNe.innerHTML);
+
+        expect(projBtnLabelNe.innerHTML).toEqual('All projects');
+
+        projButtonNe.click();
+        testHostFixture.detectChanges();
+
+        const projMenuPanelDe = projButtonDe.query(By.css('div.mat-menu-panel'));
+        const projMenuPanelNe = projMenuPanelDe.nativeElement;
+
+        expect(projMenuPanelNe).toBeDefined();
+
+    });
+
+    it('should select one project in the menu panel', () => {
+
+        const projButtonDe = fulltextSearchComponentDe.query(By.css('button.kui-project-filter-button'));
+        const projButtonNe = projButtonDe.nativeElement;
+
+        const projBtnLabelDe = projButtonDe.query(By.css('button > p.label'));
+        const projBtnLabelNe = projBtnLabelDe.nativeElement;
+
+        projButtonNe.click();
+        testHostFixture.detectChanges();
+
+        const projMenuPanelDe = projButtonDe.query(By.css('div.mat-menu-panel'));
+        const projPanelItemDe = projMenuPanelDe.query(By.css('.project-item'));
+        const projPanelItemNe = projPanelItemDe.nativeElement;
+        spyOn(testHostComponent.fulltextSearch, 'setProject').and.callThrough();
+        spyOn(testHostComponent.fulltextSearch, 'changeFocus').and.callThrough();
+
+        projPanelItemNe.click();
+        testHostFixture.detectChanges();
+
+        expect(projBtnLabelNe.innerHTML).toEqual('anything');
+        expect(testHostComponent.fulltextSearch.setProject).toHaveBeenCalledTimes(1);
+        expect(testHostComponent.fulltextSearch.setProject).toHaveBeenCalledWith(testHostComponent.fulltextSearch.projects[0]);
+        expect(testHostComponent.fulltextSearch.changeFocus).toHaveBeenCalledTimes(1);
 
     });
 
