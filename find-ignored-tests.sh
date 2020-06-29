3667feafcb97e36ebcd4b5624b065f2021766462
Tobias Schweizer <t.schweizer@unibas.ch>
2020-06-29 17:57:14 +0200

    chore (ignored tests): catch xdescribe

--- find-ignored-tests.sh
+++ find-ignored-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # find deactivated tests
-hits=`find projects/dsp-ui -name "*.spec.ts" | xargs grep 'fit\|fdescribe\|xit' | wc -l`
+hits=`find projects/dsp-ui -name "*.spec.ts" | xargs grep 'fit\|fdescribe\|xdescribe\|xit' | wc -l`
 if [ $hits -ne 0 ];
  then
   echo "Deactivated tests found: ", `find projects/dsp-ui -name "*.spec.ts" | xargs grep 'fit\|fdescribe\|xit'`
