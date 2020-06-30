929450e8c561b5c66bef03bef5e1b0b1a252eb01
flaurens <flavie.laurens@unibas.ch>
2020-06-30 11:48:54 +0200

    test(expert-search): Add missing imports and providers

--- projects/dsp-ui/src/lib/search/expert-search/expert-search.component.spec.ts
+++ projects/dsp-ui/src/lib/search/expert-search/expert-search.component.spec.ts
@@ -1,14 +1,37 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { KnoraApiConfig } from '@dasch-swiss/dsp-js';
+import { DspApiConfigToken } from '../../core/core.module';
 import { ExpertSearchComponent } from './expert-search.component';
 
 describe('ExpertSearchComponent', () => {
   let component: ExpertSearchComponent;
   let fixture: ComponentFixture<ExpertSearchComponent>;
 
+  const dspConfSpy = new KnoraApiConfig('http', 'localhost', 3333, undefined, undefined, true);
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ExpertSearchComponent ]
+      declarations: [ ExpertSearchComponent ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule
+      ],
+      providers: [
+        {
+            provide: DspApiConfigToken,
+            useValue: dspConfSpy
+        },
+        {
+            provide: ActivatedRoute,
+            useValue: {
+              params: null
+            }
+          }
+      ]
     })
     .compileComponents();
   }));
