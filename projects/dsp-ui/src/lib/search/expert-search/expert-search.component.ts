a0c6a94eca0ac6ba729dab3386fed9adfdd562ea
flaurens <flavie.laurens@unibas.ch>
2020-06-30 11:22:41 +0200

    feat(search): Expert search component

--- projects/dsp-ui/src/lib/search/expert-search/expert-search.component.ts
+++ projects/dsp-ui/src/lib/search/expert-search/expert-search.component.ts
@@ -1,15 +1,126 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Inject, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { KnoraApiConfig } from '@dasch-swiss/dsp-js';
+import { DspApiConfigToken } from '../../core/core.module';
 
 @Component({
-  selector: 'dsp-expert-search',
-  templateUrl: './expert-search.component.html',
-  styleUrls: ['./expert-search.component.scss']
+    selector: 'dsp-expert-search',
+    templateUrl: './expert-search.component.html',
+    styleUrls: ['./expert-search.component.scss']
 })
 export class ExpertSearchComponent implements OnInit {
 
-  constructor() { }
+    /**
+     * @param route Route to navigate after search. This route path should contain a component for search results.
+     */
+    @Input() route?;
 
-  ngOnInit(): void {
-  }
+    /**
+     * @param gravsearch Send the gravsearch query back.
+     */
+    @Output() gravsearch = new EventEmitter<string>();
+
+    /**
+     * @param toggleExtendedSearchForm Trigger toggle for extended search form.
+     */
+    @Output() toggleExpertSearchForm = new EventEmitter<boolean>();
+
+    expertSearchForm: FormGroup;
+
+    constructor(
+        @Inject(DspApiConfigToken) private dspApiConfig: KnoraApiConfig,
+        private fb: FormBuilder,
+        private _route: ActivatedRoute,
+        private _router: Router
+    ) { }
+
+    ngOnInit(): void {
+        this.initForm();
+    }
+
+    /**
+     * @ignore
+     * Initiate the form with predefined Gravsearch query as example.
+     */
+    private initForm() {
+        this.expertSearchForm = this.fb.group({
+            gravquery: [
+                `
+PREFIX knora-api: <http://api.knora.org/ontology/knora-api/simple/v2#>
+PREFIX incunabula: <${this.dspApiConfig.apiUrl}/ontology/0803/incunabula/simple/v2#>
+
+CONSTRUCT {
+    ?book knora-api:isMainResource true .
+    ?book incunabula:title ?title .
+
+} WHERE {
+    ?book a incunabula:book .
+    ?book incunabula:title ?title .
+}
+`,
+                Validators.required
+            ]
+        });
+    }
+
+    /**
+     * @ignore
+     * Reset the form to the initial state.
+     */
+    resetForm() {
+        this.initForm();
+    }
+
+    /**
+     * @ignore
+     * Send the gravsearch query to the result view, either through the route or by emitting the gravsearch as an output.
+     */
+    submitQuery() {
+        const gravsearch = this.generateGravsearch(0);
+
+        // does it make sense to keep this._router.navigate(...)??
+        if (this.route) {
+            this._router.navigate([this.route + '/extended/', gravsearch], { relativeTo: this._route });
+        } else {
+            this.gravsearch.emit(gravsearch);
+        }
+
+        // toggle expert search form
+        this.toggleExpertSearchForm.emit(true);
+    }
+
+    /**
+     * @ignore
+     * Generate the whole gravsearch query matching the query given by the form.
+     */
+    private generateGravsearch(offset: number = 0): string {
+        const queryTemplate = this.expertSearchForm.controls['gravquery'].value;
+
+        // offset component of the Gravsearch query
+        const offsetTemplate = `
+         OFFSET ${offset}
+         `;
+
+        // function that generates the same Gravsearch query with the given offset
+        const generateGravsearchWithCustomOffset = (
+            localOffset: number
+        ): string => {
+            const offsetCustomTemplate = `
+             OFFSET ${localOffset}
+             `;
+
+            return queryTemplate + offsetCustomTemplate;
+        };
+
+        if (offset === 0) {
+            // store the function so another Gravsearch query can be created with an increased offset
+            // TODO: replace _searchParamsService with the new way fron DSP-JS
+            /* this._searchParamsService.changeSearchParamsMsg(
+                new ExtendedSearchParams(generateGravsearchWithCustomOffset) // caution: need to unsubscribe to this behaviour subject
+            ); */
+        }
+        return queryTemplate + offsetTemplate;
+    }
 
 }
--- projects/dsp-ui/src/lib/search/search.module.ts
+++ projects/dsp-ui/src/lib/search/search.module.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -10,6 +12,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { ExpertSearchComponent } from './expert-search/expert-search.component';
 import { FulltextSearchComponent } from './fulltext-search/fulltext-search.component';
 import { SearchPanelComponent } from './search-panel/search-panel.component';
+
 
 
 @NgModule({
@@ -21,7 +24,10 @@ import { SearchPanelComponent } from './search-panel/search-panel.component';
     imports: [
         CommonModule,
         FormsModule,
+        ReactiveFormsModule,
         MatIconModule,
+        MatInputModule,
+        MatFormFieldModule,
         MatMenuModule,
         MatListModule,
         MatButtonModule,
