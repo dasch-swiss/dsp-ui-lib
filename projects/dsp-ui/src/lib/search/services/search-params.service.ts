7f33ce03e9584c54b96beebd630a0f99d144137b
Tobias Schweizer <t.schweizer@unibas.ch>
2020-07-01 14:24:41 +0200

    feature (search): add Gravsearch Generation Service

---
+++ projects/dsp-ui/src/lib/search/services/search-params.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+/*
+ * Represents the parameters of an extended search.
+ */
+export class ExtendedSearchParams {
+
+    /**
+     *
+     * @param generateGravsearch a function that generates a Gravsearch query.
+     *
+     * The function takes the offset
+     * as a parameter and returns a Gravsearch query string.
+     * Returns false if not set correctly (init state).
+     */
+    constructor(public generateGravsearch: (offset: number) => string | boolean) {
+
+    }
+
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchParamsService {
+
+    private _currentSearchParams;
+
+    constructor() {
+        // init with a dummy function that returns false
+        // if the application is reloaded, this will be returned
+        this._currentSearchParams = new BehaviorSubject<ExtendedSearchParams>(new ExtendedSearchParams((offset: number) => false));
+    }
+
+    /**
+     * Updates the parameters of an extended search.
+     *
+     * @param ExtendedSearchParams searchParams
+     * @returns void
+     */
+    changeSearchParamsMsg(searchParams: ExtendedSearchParams): void {
+        this._currentSearchParams.next(searchParams);
+    }
+
+    /**
+     * Gets the search params of an extended search.
+     *
+     */
+    getSearchParams(): ExtendedSearchParams {
+        return this._currentSearchParams.getValue();
+    }
+
+}
