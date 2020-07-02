194e47382ba70b6b18d0d959e80dc63c7b8f09b6
Tobias Schweizer <t.schweizer@unibas.ch>
2020-07-02 10:31:27 +0200

    chore (script): find all ignored tests

--- find-ignored-tests.sh
+++ find-ignored-tests.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # find deactivated tests
-hits=`find projects/dsp-ui -name "*.spec.ts" | xargs grep 'fit\|fdescribe\|xdescribe\|xit' | wc -l`
+hits=`find . -name "*spec.ts" | xargs grep 'fit\|fdescribe\|xdescribe\|xit' | wc -l`
 if [ $hits -ne 0 ];
  then
-  echo "Deactivated tests found: ", `find projects/dsp-ui -name "*.spec.ts" | xargs grep 'fit\|fdescribe\|xdescribe\|xit'`
+  echo "Deactivated tests found: ", `find . -name "*spec.ts" | xargs grep 'fit\|fdescribe\|xdescribe\|xit'`
   exit 1
 fi
