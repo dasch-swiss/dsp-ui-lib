cc10d792e9bde22eca3d2eef9c0c14db1de77501
mdelez <60604010+mdelez@users.noreply.github.com>
2020-07-08 12:55:03 +0200

    tests (event-bus): added tests for the event bus

--- projects/dsp-ui/src/lib/viewer/services/event-bus.service.spec.ts
+++ projects/dsp-ui/src/lib/viewer/services/event-bus.service.spec.ts
@@ -1,16 +1,35 @@
 import { TestBed } from '@angular/core/testing';
-
-import { EventBusService } from './event-bus.service';
+import { EmitEvent, EventBusService, Events } from './event-bus.service';
 
 describe('EventBusService', () => {
-  let service: EventBusService;
+    let service: EventBusService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(EventBusService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(EventBusService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should listen for events and execute code in the callback when the event is emitted', () => {
+        let valuesCount = 2;
+
+        // listen for ValueAdded event
+        service.on(Events.ValueAdded, () => valuesCount += 1);
+
+        // listen for ValueAdded event
+        service.on(Events.ValueDeleted, () => valuesCount -= 1);
+
+        // emit ValueAdded event
+        service.emit(new EmitEvent(Events.ValueAdded));
+
+        expect(valuesCount).toEqual(3);
+
+        // emit ValueDeleted event
+        service.emit(new EmitEvent(Events.ValueDeleted));
+
+        expect(valuesCount).toEqual(2);
+    });
 });
