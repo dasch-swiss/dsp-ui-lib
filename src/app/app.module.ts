6599fc2cce100f3f419614d01904cf929161225f
flaurens <flavie.laurens@unibas.ch>
2020-07-08 14:28:22 +0200

    refactor(app): Fix playground

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatJDNConvertibleCalendarDateAdapterModule } from 'jdnconvertiblecalendardateadapter';
-import { ActionPlaygroundComponent } from './action-playground/action-playground.component';
-import { AppInitService } from './app-init.service';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ModifyComponent } from './modify/modify.component';
-import { ReadComponent } from './read/read.component';
 import {
     DspActionModule,
     DspApiConfigToken,
@@ -18,9 +11,16 @@ import {
     DspSearchModule,
     DspViewerModule
 } from '@dasch-swiss/dsp-ui';
+import { MatJDNConvertibleCalendarDateAdapterModule } from 'jdnconvertiblecalendardateadapter';
+import { ActionPlaygroundComponent } from './action-playground/action-playground.component';
+import { AdvancedSearchPlaygroundComponent } from './advanced-search-playground/advanced-search-playground.component';
+import { AppInitService } from './app-init.service';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ModifyComponent } from './modify/modify.component';
+import { ReadComponent } from './read/read.component';
 import { SearchPlaygroundComponent } from './search-playground/search-playground.component';
 import { SearchResultsComponent } from './search-playground/search-results/search-results.component';
-import { AdvancedSearchPlaygroundComponent } from './advanced-search-playground/advanced-search-playground.component';
 
 
 export function initializeApp(appInitService: AppInitService) {
--- src/app/search-playground/search-playground.component.ts
+++ src/app/search-playground/search-playground.component.ts
@@ -7,32 +7,10 @@ import { Component } from '@angular/core';
 })
 export class SearchPlaygroundComponent {
 
-    loading: boolean = true;
-
-    gravsearchQuery: string;
-
     constructor() { }
 
-    setGravsearch(query: string) {
-
-        this.loading = true;
-
-        /* this._cache.del('gravsearch');
-
-        this._cache.set('gravsearch', query);
-
-        this._cache.get('gravsearch').subscribe(
-            (response: string) => {
-                // get cached query
-                this.gravsearchQuery = response;
-            },
-            (error: any) => {
-                console.error(error);
-            }
-        ); */
-
-        this.loading = false;
-
+    submitQuery(query: string) {
+        console.log('output', query);
     }
 
 }
