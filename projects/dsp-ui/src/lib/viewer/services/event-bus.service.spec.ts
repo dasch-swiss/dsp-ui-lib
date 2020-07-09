d418ed80887530468760d595bca6835f8cd13676
mdelez <60604010+mdelez@users.noreply.github.com>
2020-07-09 13:33:14 +0200

    test (event-bus): added test for unsubscribing

--- projects/dsp-ui/src/lib/viewer/services/event-bus.service.spec.ts
+++ projects/dsp-ui/src/lib/viewer/services/event-bus.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { Subscription } from 'rxjs';
 import { EmitEvent, EventBusService, Events } from './event-bus.service';
 
 describe('EventBusService', () => {
@@ -21,7 +22,7 @@ describe('EventBusService', () => {
         // listen for ValueAdded event
         service.on(Events.ValueAdded, () => valuesCount += 1);
 
-        // listen for ValueAdded event
+        // listen for ValueDeleted event
         service.on(Events.ValueDeleted, () => valuesCount -= 1);
 
         // emit ValueAdded event
@@ -33,5 +34,26 @@ describe('EventBusService', () => {
         service.emit(new EmitEvent(Events.ValueDeleted));
 
         expect(valuesCount).toEqual(2);
+    });
+
+    it('should no longer execute the callback code when an event is emitted after unsubscribing', () => {
+        let valuesCount = 2;
+        let eventBusSubscription: Subscription;
+
+        // listen for ValueAdded event
+        eventBusSubscription = service.on(Events.ValueAdded, () => valuesCount += 1);
+
+        // emit ValueAdded event
+        service.emit(new EmitEvent(Events.ValueAdded));
+
+        expect(valuesCount).toEqual(3);
+
+        eventBusSubscription.unsubscribe();
+
+        // emit ValueAdded event again, this time it should not trigger the callback
+        service.emit(new EmitEvent(Events.ValueAdded));
+
+        expect(valuesCount).toEqual(3);
+
     });
 });
