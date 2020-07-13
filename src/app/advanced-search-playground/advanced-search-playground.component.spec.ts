6e530a0b87567d2a2012efaddb4f8b252baddd57
Tobias Schweizer <tobiasschweizer@users.noreply.github.com>
2020-07-13 13:32:33 +0200

    Flaky E2E Tests (#121)
    * tests (e2e): wait for Knora to return a response
    
    * tests (e2e): wait for Knora to return a response
    
    * tests (e2e): wait for element to be present
    
    * tests (e2e): increase timeout
    
    * tests (e2e): wait for element to be present
    
    * tests (e2e): use alternative way to select option
    
    * tests (e2e): logout before doing an advanced search
    
    * test (search): adapt link prop test
    
    * fix (playground): fix test deps
    
    * tests (e2e): wait for element to be present

--- src/app/advanced-search-playground/advanced-search-playground.component.spec.ts
+++ src/app/advanced-search-playground/advanced-search-playground.component.spec.ts
@@ -1,7 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdvancedSearchPlaygroundComponent } from './advanced-search-playground.component';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { DspApiConnectionToken } from '@dasch-swiss/dsp-ui';
+import { ApiResponseData, AuthenticationEndpointV2, LogoutResponse } from '@dasch-swiss/dsp-js';
+import { of } from 'rxjs';
 
 /**
  * Test host component to simulate parent component.
@@ -10,26 +13,45 @@ import { Component, OnInit } from '@angular/core';
     selector: 'dsp-advanced-search',
     template: ``
 })
-class TestHostComponent {}
+class TestHostComponent {
+}
 
 describe('AdvancedSearchPlaygroundComponent', () => {
-  let component: AdvancedSearchPlaygroundComponent;
-  let fixture: ComponentFixture<AdvancedSearchPlaygroundComponent>;
+    let component: AdvancedSearchPlaygroundComponent;
+    let fixture: ComponentFixture<AdvancedSearchPlaygroundComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AdvancedSearchPlaygroundComponent, TestHostComponent ]
-    })
-    .compileComponents();
-  }));
+    const authSpyObj = {
+        v2: {
+            auth: jasmine.createSpyObj('auth', ['logout'])
+        }
+    };
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AdvancedSearchPlaygroundComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [AdvancedSearchPlaygroundComponent, TestHostComponent],
+            providers: [
+                {
+                    provide: DspApiConnectionToken,
+                    useValue: authSpyObj
+                }
+            ]
+        })
+            .compileComponents();
+    }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        const authSpy = TestBed.inject(DspApiConnectionToken);
+
+        (authSpy.v2.auth as jasmine.SpyObj<AuthenticationEndpointV2>).logout.and.returnValue(
+            of({} as ApiResponseData<LogoutResponse>)
+        );
+
+        fixture = TestBed.createComponent(AdvancedSearchPlaygroundComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
