00d49a37e384b98de437ceadf859c21dbefc11a4
mdelez <60604010+mdelez@users.noreply.github.com>
2020-07-16 16:44:20 +0200

    first commit

--- projects/dsp-ui/src/lib/action/action.module.ts
+++ projects/dsp-ui/src/lib/action/action.module.ts
@@ -1,12 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MessageComponent } from './components/message/message.component';
 import { ProgressIndicatorComponent } from './components/progress-indicator/progress-indicator.component';
 import { SortButtonComponent } from './components/sort-button/sort-button.component';
+import { StringLiteralInputComponent } from './components/string-literal-input/string-literal-input.component';
 import { AdminImageDirective } from './directives/admin-image/admin-image.directive';
 import { ExistingNameDirective } from './directives/existing-names/existing-name.directive';
 import { GndDirective } from './directives/gnd/gnd.directive';
@@ -28,14 +33,21 @@ import { TruncatePipe } from './pipes/string-transformation/truncate.pipe';
     GndDirective,
     ProgressIndicatorComponent,
     MessageComponent,
-    SortButtonComponent
+    SortButtonComponent,
+
+    StringLiteralInputComponent
   ],
   imports: [
     CommonModule,
     MatIconModule,
+    MatInputModule,
     MatMenuModule,
     MatCardModule,
-    MatListModule
+    MatListModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    FormsModule
   ],
   exports: [
     FormattedBooleanPipe,
@@ -47,7 +59,9 @@ import { TruncatePipe } from './pipes/string-transformation/truncate.pipe';
     ExistingNameDirective,
     ProgressIndicatorComponent,
     MessageComponent,
-    SortButtonComponent
+    SortButtonComponent,
+
+    StringLiteralInputComponent
   ]
 })
 
--- projects/dsp-ui/src/lib/action/index.ts
+++ projects/dsp-ui/src/lib/action/index.ts
@@ -15,6 +15,8 @@ export * from './components/progress-indicator/progress-indicator.component';
 export * from './components/message/message.component';
 export * from './components/sort-button/sort-button.component';
 
+export * from './components/string-literal-input/string-literal-input.component';
+
 // directives
 export * from './directives/admin-image/admin-image.directive';
 export * from './directives/existing-names/existing-name.directive';
