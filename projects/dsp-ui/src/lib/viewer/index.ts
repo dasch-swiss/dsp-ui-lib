c94d1f4dbc74f016b10c2f8b66f1e373f35e523f
Tobias Schweizer <tobiasschweizer@users.noreply.github.com>
2020-07-27 11:24:52 +0200

    Add Still Image Viewer (#128)
    * feature (viewer): add still image viewer (ongoing)
    
    * fix (viewer): export component class
    
    * feature (playground): add still image to playground
    
    * tests (viewer): add test for still image
    
    * tests (viewer): add test for still image component
    
    * refactor (viewer): rename private member
    
    * refactor (viewer): use DSP prefix in button names
    
    * refactor (viewer): deactivate sequence mode in still image viewer
    
    * tests (viewer): check for image caption in still image viewer
    
    * feature (viewer): add public method to image viewer
    
    * feature (viewer): support regions
    
    * feature (viewer): add logic for region handling
    
    * feature (viewer): add logic for region handling
    
    * feature (viewer): activate a region via @Input
    
    * tests (viewer): adapt spec for still image comp.
    
    * tests (viewer): fix imports
    
    * tests (viewer): adapt spec for still image comp.
    
    * tests (playground): adapt spec for still image comp.
    
    * tests (viewer): fix imports
    
    * tests (playground): more regions for still image comp.
    
    * tests (viewer): test @Output of still image comp.
    
    * tests (viewer): test highlight region of still image comp.
    
    * tests (viewer): test activate region of still image comp.
    
    * tests (viewer): doo not use non exported member from dsp-js
    
    * feature (playground): test activate region of still image comp.
    
    * refactor (viewer): coding conventions for private methods and members
    
    * refactor (viewer): move region sorting logic to a method
    
    * feature (playground): test activate region of still image comp.
    
    * feature (playground): test activate region of still image comp.
    
    * chore (viewer): add link to DSP task for test data
    
    * refactor (viewer): fix spacing

--- projects/dsp-ui/src/lib/viewer/index.ts
+++ projects/dsp-ui/src/lib/viewer/index.ts
@@ -23,3 +23,5 @@ export * from './views/resource-view/resource-view.component';
 export * from './views/property-view/property-view.component';
 export * from './views/list-view/resource-list/resource-list.component';
 export * from './views/list-view/resource-grid/resource-grid.component';
+// media representations
+export * from './representation/still-image/still-image.component';
---
+++ src/app/still-image/still-image-playground.component.ts
@@ -0,0 +1,90 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import {
+    ApiResponseData, ApiResponseError, Constants,
+    KnoraApiConnection,
+    LogoutResponse, ReadGeomValue,
+    ReadResource,
+    ReadStillImageFileValue, ReadValue
+} from '@dasch-swiss/dsp-js';
+import { DspApiConnectionToken, Region, StillImageRepresentation } from '@dasch-swiss/dsp-ui';
+import { mergeMap } from 'rxjs/operators';
+
+// TODO: get test data from Knora resource
+class Geom extends ReadValue {
+    geometryString: string;
+}
+
+@Component({
+    selector: 'app-still-image',
+    templateUrl: './still-image-playground.component.html',
+    styleUrls: ['./still-image-playground.component.scss']
+})
+export class StillImagePlaygroundComponent implements OnInit {
+
+    resourceIri = 'http://rdfh.ch/0803/00014b43f902'; // incunabula, TODO: use resource from anything project https://dasch.myjetbrains.com/youtrack/issue/DSP-506
+    loading: boolean;
+
+    stillImageRepresentations: StillImageRepresentation[];
+    caption = 'test image';
+
+    actReg: string;
+
+    constructor(@Inject(DspApiConnectionToken) private knoraApiConnection: KnoraApiConnection) {
+    }
+
+    ngOnInit(): void {
+        this.loading = true;
+
+        this.knoraApiConnection.v2.auth.logout().pipe(
+            mergeMap(
+                (logoutRes: ApiResponseData<LogoutResponse> | ApiResponseError) => {
+                    if (logoutRes instanceof ApiResponseData) {
+                        return this.knoraApiConnection.v2.res.getResource(this.resourceIri);
+                    } else {
+                        throw logoutRes;
+                    }
+                })).subscribe(
+            (res: ReadResource) => {
+
+                // TODO: remove dummy region
+                const geomStr
+                    = '{"status":"active","lineColor":"#ff3333","lineWidth":2,"points":[{"x":0.0989010989010989,"y":0.18055555555555555},{"x":0.7252747252747253,"y":0.7245370370370371}],"type":"rectangle"}';
+
+                const parseReg = new Geom();
+                parseReg.geometryString = geomStr;
+
+                const geometry = new ReadGeomValue(parseReg);
+
+                const geomStr2
+                    = '{"status":"active","lineColor":"#ff3333","lineWidth":2,"points":[{"x":0.1989010989010989,"y":0.18055555555555555},{"x":0.7252747252747253,"y":0.7245370370370371}],"type":"rectangle"}';
+
+                const parseReg2 = new Geom();
+                parseReg2.geometryString = geomStr2;
+
+                const geometry2 = new ReadGeomValue(parseReg2);
+
+                const regionRes = new ReadResource();
+                regionRes.id = 'activeRegion';
+                regionRes.label = 'test region ';
+                regionRes.properties[Constants.HasGeometry] = [geometry];
+
+                const regionRes2 = new ReadResource();
+                regionRes2.id = 'activeRegion2';
+                regionRes2.label = 'test region 2';
+                regionRes2.properties[Constants.HasGeometry] = [geometry2];
+
+                this.stillImageRepresentations
+                    = [new StillImageRepresentation(res.getValuesAs('http://api.knora.org/ontology/knora-api/v2#hasStillImageFileValue', ReadStillImageFileValue)[0], [new Region(regionRes), new Region(regionRes2)])];
+
+                this.loading = false;
+            },
+            err => {
+                console.error(err);
+            }
+        );
+    }
+
+    regionClicked(regionIri: string) {
+        console.log(regionIri);
+    }
+}
