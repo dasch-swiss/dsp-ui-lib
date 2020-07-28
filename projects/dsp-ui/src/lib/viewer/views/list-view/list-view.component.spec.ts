8db03ba7e91c461d0dccfec2b77717ce42012c38
flaurens <flavie.laurens@unibas.ch>
2020-07-28 10:27:57 +0200

    fix(list-view): Move searchSpy in beforeEach

--- projects/dsp-ui/src/lib/viewer/views/list-view/list-view.component.spec.ts
+++ projects/dsp-ui/src/lib/viewer/views/list-view/list-view.component.spec.ts
@@ -4,11 +4,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { CountQueryResponse, MockResource, ReadResourceSequence, SearchEndpointV2 } from '@dasch-swiss/dsp-js';
+import { IFulltextSearchParams } from '@dasch-swiss/dsp-js/src/api/v2/search/search-endpoint-v2';
 import { of } from 'rxjs';
 import { DspApiConnectionToken } from '../../../core';
 import { ListViewComponent, SearchParams } from './list-view.component';
-import { IFulltextSearchParams } from '@dasch-swiss/dsp-js/src/api/v2/search/search-endpoint-v2';
 
+// TODO: declare this component in the TestBed declarations, e.g. advanced-search comp.
 /**
  * Test host component to simulate child component, here resource-list.
  */
@@ -88,52 +89,61 @@ describe('ListViewComponent', () => {
     }));
 
     beforeEach(() => {
+
+        // mock doFulltextSearchCountQuery and doFulltextSearch response before creating the component
+        // TODO: check fulltext-search comp. spec as an example
+        const searchSpy = TestBed.inject(DspApiConnectionToken);
+
+        (searchSpy.v2.search as jasmine.SpyObj<SearchEndpointV2>).doFulltextSearchCountQuery.and.callFake(
+            () => {
+                const num = new CountQueryResponse();
+                num.numberOfResults = 5;
+                return of(num);
+            }
+        );
+
+        (searchSpy.v2.search as jasmine.SpyObj<SearchEndpointV2>).doFulltextSearch.and.callFake(
+            (searchTerm: string, offset?: number, params?: IFulltextSearchParams) => {
+
+                let resources: ReadResourceSequence;
+                // mock list of resourcses to simulate full-text search response
+                MockResource.getTesthings(5).subscribe(res => {
+                    console.log(res);
+                    resources = res;
+                });
+                if (resources.resources.length) {
+                    return of(resources);
+                }
+            }
+        );
+
         testHostFixture = TestBed.createComponent(TestParentComponent);
         testHostComponent = testHostFixture.componentInstance;
         testHostFixture.detectChanges();
 
-
         expect(testHostComponent).toBeTruthy();
     });
 
-    // it('should do fulltext search', () => {
+    it('should do fulltext search', () => {
 
-    //     const searchSpy = TestBed.inject(DspApiConnectionToken);
+        const searchSpy = TestBed.inject(DspApiConnectionToken);
 
-    //     (searchSpy.v2.search as jasmine.SpyObj<SearchEndpointV2>).doFulltextSearchCountQuery.and.callFake(
-    //         () => {
-    //             const num = new CountQueryResponse();
-    //             num.numberOfResults = 5;
-    //             return of(num);
-    //         }
-    //     );
+        expect(
+            searchSpy.v2.search.doFulltextSearchCountQuery
+        ).toHaveBeenCalledWith(
+            'fake query',
+            0,
+            { limitToProject: 'http://rdfh.ch/projects/0803' }
+        );
+        // expect(testComponent.resources.resources.length).toEqual(5);
+        // expect(testHostComponent.inputValueComponent.resources[0].id).toEqual('http://rdfh.ch/0001/IwMDbs0KQsaxSRUTl2cAIQ');
 
-    //     (searchSpy.v2.search as jasmine.SpyObj<SearchEndpointV2>).doFulltextSearch.and.callFake(
-    //         (searchTerm: string, offset?: number, params?: IFulltextSearchParams) => {
-
-    //             let resources: ReadResourceSequence;
-    //             // mock list of resourcses to simulate full-text search response
-    //             MockResource.getTesthings(5).subscribe(res => {
-    //                 console.log(res);
-    //                 resources = res;
-    //             });
-    //             if (resources.resources.length) {
-    //                 return of(resources);
-    //             }
-    //         }
-    //     );
-
-
-    //     expect(searchSpy.v2.search.doFulltextSearchCountQuery).toHaveBeenCalledWith('fake query', 0, { limitToProject: 'http://rdfh.ch/projects/0803' });
-    //     // expect(testComponent.resources.resources.length).toEqual(5);
-    //     //   expect(testHostComponent.inputValueComponent.resources[0].id).toEqual('http://rdfh.ch/0001/IwMDbs0KQsaxSRUTl2cAIQ');
-
-    //     // {
-    //     //     "schema:numberOfItems" : 2,
-    //     //     "@context" : {
-    //     //       "schema" : "http://schema.org/"
-    //     //     }
-    //     //   }
-    // });
+        // {
+        //     "schema:numberOfItems" : 2,
+        //     "@context" : {
+        //       "schema" : "http://schema.org/"
+        //     }
+        //   }
+    });
 
 });
