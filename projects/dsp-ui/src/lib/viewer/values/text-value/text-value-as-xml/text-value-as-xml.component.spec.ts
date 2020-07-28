2dd25f094ddf00b2772f4e2dd184fcad5ca7dc6f
Tobias Schweizer <t.schweizer@unibas.ch>
2020-07-28 11:17:40 +0200

    tests (viewer): add test for XML value comp. (ongoing)

--- projects/dsp-ui/src/lib/viewer/values/text-value/text-value-as-xml/text-value-as-xml.component.spec.ts
+++ projects/dsp-ui/src/lib/viewer/values/text-value/text-value-as-xml/text-value-as-xml.component.spec.ts
@@ -1,25 +1,113 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TextValueAsXMLComponent } from './text-value-as-xml.component';
+import { Component, DebugElement, forwardRef, Input, OnInit, ViewChild } from '@angular/core';
+import { ReadTextValueAsXml } from '@dasch-swiss/dsp-js';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
+import { DateValueComponent } from '../../..';
+
+/**
+ * Test host component to simulate parent component.
+ */
+@Component({
+    selector: 'ckeditor',
+    template: ``,
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            multi: true,
+            useExisting: forwardRef(() => TestCKEditorComponent),
+        }
+    ]
+})
+class TestCKEditorComponent implements ControlValueAccessor {
+
+    @Input() config;
+
+    @Input() editor;
+
+    constructor() {
+    }
+
+    writeValue(obj: any) {
+    }
+
+    registerOnChange(fn: any) {
+    }
+
+    registerOnTouched(fn: any) {
+    }
+
+}
+
+/**
+ * Test host component to simulate parent component.
+ */
+@Component({
+    template: `
+        <dsp-text-value-as-xml #inputVal [displayValue]="displayInputVal" [mode]="mode"></dsp-text-value-as-xml>`
+})
+class TestHostDisplayValueComponent implements OnInit {
+
+    @ViewChild('inputVal') inputValueComponent: TextValueAsXMLComponent;
+
+    displayInputVal: ReadTextValueAsXml;
+
+    mode: 'read' | 'update' | 'create' | 'search';
+
+    ngOnInit() {
+
+        const inputVal = new ReadTextValueAsXml();
+        inputVal.mapping = 'http://rdfh.ch/standoff/mappings/StandardMapping';
+        inputVal.xml = '<html><p>my text</p></html>';
+
+        this.displayInputVal = inputVal;
+
+        this.mode = 'read';
+
+    }
+}
 
 describe('TextValueAsXMLComponent', () => {
-  let component: TextValueAsXMLComponent;
-  let fixture: ComponentFixture<TextValueAsXMLComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TextValueAsXMLComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                TextValueAsXMLComponent,
+                TestHostDisplayValueComponent,
+                TestCKEditorComponent
+            ],
+            imports: [
+                ReactiveFormsModule,
+                BrowserAnimationsModule
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TextValueAsXMLComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    describe('display and edit a text value with xml markup', () => {
+        let testHostComponent: TestHostDisplayValueComponent;
+        let testHostFixture: ComponentFixture<TestHostDisplayValueComponent>;
+        let ckeditorDe: DebugElement;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        beforeEach(() => {
+            testHostFixture = TestBed.createComponent(TestHostDisplayValueComponent);
+            testHostComponent = testHostFixture.componentInstance;
+            testHostFixture.detectChanges();
+
+            const hostCompDe = testHostFixture.debugElement;
+
+            ckeditorDe = hostCompDe.query(By.directive(TestCKEditorComponent));
+        });
+
+        it('should display an existing value', () => {
+
+            expect(testHostComponent.inputValueComponent.displayValue.xml).toEqual('<html><p>my text</p></html>');
+
+        });
+
+    });
+
 });
