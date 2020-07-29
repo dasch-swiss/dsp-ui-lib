0d5d4072132cbea035155bea930ce3f37571e467
Tobias Schweizer <t.schweizer@unibas.ch>
2020-07-29 15:52:40 +0200

    feature (playground): log message when attempting to submit an identical text

--- src/app/ckeditor/ckeditor-playground/ckeditor-playground.component.ts
+++ src/app/ckeditor/ckeditor-playground/ckeditor-playground.component.ts
@@ -79,6 +79,8 @@ export class CkeditorPlaygroundComponent implements OnInit {
                 }
             );
 
+        } else {
+            console.log('identical, no update performed');
         }
     }
 
