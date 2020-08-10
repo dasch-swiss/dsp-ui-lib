1969690296f1d1615cc7721df89401bba9690656
mdelez <60604010+mdelez@users.noreply.github.com>
2020-08-10 15:38:39 +0200

    Action Module Confirmation Dialog Component (#150)
    * feature (confirm dialog): generated component
    
    * feature (confirm dialog): first implementation of confirmation dialog and integration into the playground
    
    * tests (confirm dialog): tests written for confirm dialog
    
    * css (confirm dialog): added some css in accordance to best practices
    
    * refactor (confirm dialog): added type assertion to remove 'any' type from constructor and added underscores to the beginning of the private variables
    
    * refactor (confirm dialog): small refactor to directly use the data provided to the dialog component instead of adding them to local variables
    
    * css (playground): small change to the playground to make the confirm dialog result message more visible

---
+++ projects/dsp-ui/src/lib/action/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,29 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+export class ConfirmationDialogData {
+    title: string;
+    message: string;
+    buttonTextOk: string;
+    buttonTextCancel: string;
+}
+
+@Component({
+  selector: 'dsp-confirmation-dialog',
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrls: ['./confirmation-dialog.component.scss']
+})
+export class ConfirmationDialogComponent {
+
+    // type assertion doesn't seem to be enforced
+    // https://stackoverflow.com/a/57787554
+    constructor(
+        @Inject(MAT_DIALOG_DATA) public data: ConfirmationDialogData,
+        private _dialogRef: MatDialogRef<ConfirmationDialogComponent>
+    ) { }
+
+    onConfirmClick(): void {
+        this._dialogRef.close(true);
+    }
+
+}
