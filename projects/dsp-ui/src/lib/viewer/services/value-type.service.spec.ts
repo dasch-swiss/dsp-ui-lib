e5db24806de2822456e910b9fc8e92a987ead8a0
mdelez <60604010+mdelez@users.noreply.github.com>
2020-09-15 11:09:46 +0200

    Fix compareObjectTypeWithValueType Method (#175)
    * fix (value-type service): added the missing conditions and added unit tests
    
    * correcting english because it's hard
    
    * tests (value-type service): added additional test to compare a text value and a non-text type

--- projects/dsp-ui/src/lib/viewer/services/value-type.service.spec.ts
+++ projects/dsp-ui/src/lib/viewer/services/value-type.service.spec.ts
@@ -46,4 +46,50 @@ describe('ValueTypeService', () => {
             expect(service.isReadOnly(valueClass)).toBeTruthy();
         });
     });
+
+    describe('compareObjectTypeWithValueType', () => {
+
+        it('should successfully compare "http://api.knora.org/ontology/knora-api/v2#TextValue" with "ReadTextValueAsString"', () => {
+            expect(service.compareObjectTypeWithValueType(
+                'ReadTextValueAsString',
+                'http://api.knora.org/ontology/knora-api/v2#TextValue'))
+                .toBeTruthy();
+        });
+
+        it('should successfully compare "http://api.knora.org/ontology/knora-api/v2#TextValue" with "ReadTextValueAsHtml"', () => {
+            expect(service.compareObjectTypeWithValueType(
+                'ReadTextValueAsHtml',
+                'http://api.knora.org/ontology/knora-api/v2#TextValue'))
+                .toBeTruthy();
+        });
+
+        it('should successfully compare "http://api.knora.org/ontology/knora-api/v2#TextValue" with "ReadTextValueAsXml"', () => {
+            expect(service.compareObjectTypeWithValueType(
+                'ReadTextValueAsXml',
+                'http://api.knora.org/ontology/knora-api/v2#TextValue'))
+                .toBeTruthy();
+        });
+
+        it('should successfully compare "http://api.knora.org/ontology/knora-api/v2#IntValue" with "http://api.knora.org/ontology/knora-api/v2#IntValue"', () => {
+            expect(service.compareObjectTypeWithValueType(
+                'http://api.knora.org/ontology/knora-api/v2#IntValue',
+                'http://api.knora.org/ontology/knora-api/v2#IntValue'))
+                .toBeTruthy();
+        });
+
+        it('should fail to compare an IntValue with a DecimalValue', () => {
+            expect(service.compareObjectTypeWithValueType(
+                'http://api.knora.org/ontology/knora-api/v2#IntValue',
+                'http://api.knora.org/ontology/knora-api/v2#DecimalValue'))
+                .toBeFalsy();
+        })
+
+        it('should fail to compare "http://api.knora.org/ontology/knora-api/v2#IntValue" with "ReadTextValueAsString"', () => {
+            expect(service.compareObjectTypeWithValueType(
+                'ReadTextValueAsString',
+                'http://api.knora.org/ontology/knora-api/v2#IntValue'))
+                .toBeFalsy();
+        });
+
+    });
 });
