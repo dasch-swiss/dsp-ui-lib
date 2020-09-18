74bd73b8c5a9555ceedc55ecee0b58445703ffec
Tobias Schweizer <tobiasschweizer@users.noreply.github.com>
2020-09-18 10:21:43 +0200

    Add CKeditor (#134)
    * feature (viewer): add CKeditor lib
    
    * feature (viewer): use custom build of CKEditor 5
    
    * feature (viewer): move CKEditor logic to value comp.
    
    * feature (playground): get new XML value
    
    * feature (viewer): export comp.
    
    * tests (viewer): add test for XML value comp. (ongoing)
    
    * tests (viewer): add test for XML value comp. (ongoing)
    
    * feature (playground): edit an XML value
    
    * feature (playground): edit an XML value
    
    * feature (viewer): process XML for CKEditor
    
    * feature (viewer): add CKEditor plugin to toolbar
    
    * feature (viewer): adapt CKEditor's headings
    
    * feature (viewer): add CKEditor redo and undo to toolbar
    
    * feature (playground): get sample XML
    
    * feature (playground): log in when page is accessed
    
    * tests (playground): mock component
    
    * tests (playground): remove fdescribe
    
    * feature (playground): log message when attempting to submit an identical text
    
    * feature (viewer): add support for internal links
    
    * feature (viewer): add support for internal links (automatic approach)
    
    * tests (viewer): use test data from dsp-js
    
    * tests (viewer): add spec to text val as XML
    
    * feature (viewer): add comment to text as XML comp.
    
    * tests (viewer): add spec to text val as XML
    
    * tests (viewer): add spec to text val as XML
    
    * tests (viewer): add spec to text val as XML
    
    * feature (playground): log XML
    
    * tests (viewer): check for markup conversion Knora - CKEDitor
    
    * tests (viewer): check for markup conversion Knora - CKEDitor
    
    * Revert "tests (viewer): check for markup conversion Knora - CKEDitor"
    
    This reverts commit 1a28fd30313171a8ed460979f9f5e19ea7bc4f80.
    
    * chore (viewer): clean up after merging
    
    * feature (ckeditor playground): improve value logging
    
    * feature (viewer): make XML transformation configurable
    
    * test (viewer): fix test for XML comp.
    
    * feature (viewer):make component more flexible
    
    * feature (viewer): improve check for config
    
    * fix (viewer): rearrange import to make Mike the Linter happy
    
    * feature (viewer): add CSS for CKEDITOR

---
+++ src/app/ckeditor/ckeditor-playground/ckeditor-playground.component.spec.ts
@@ -0,0 +1,68 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CkeditorPlaygroundComponent } from './ckeditor-playground.component';
+import { DspApiConnectionToken } from '@dasch-swiss/dsp-ui';
+import { ReadResource, ResourcesEndpointV2 } from '@dasch-swiss/dsp-js';
+import { of } from 'rxjs';
+import { ApiResponseData, AuthenticationEndpointV2, LoginResponse } from '@dasch-swiss/dsp-js/index';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { TextValueAsXMLComponent } from '../../../../projects/dsp-ui/src/lib/viewer';
+
+/**
+ * Test host component to simulate parent component.
+ */
+@Component({
+    selector: `dsp-text-value-as-xml`,
+    template: ``
+})
+class TestDspTextValueAsXMLComponent {
+
+    @Input() mode;
+
+    @Input() displayValue;
+
+}
+
+describe('CkeditorPlaygroundComponent', () => {
+    let component: CkeditorPlaygroundComponent;
+    let fixture: ComponentFixture<CkeditorPlaygroundComponent>;
+
+    beforeEach(async(() => {
+        const dspSpyObj = {
+            v2: {
+                auth: jasmine.createSpyObj('auth', ['login']),
+                res: jasmine.createSpyObj('res', ['getResource']),
+                values: jasmine.createSpyObj('values', ['updateValue', 'getValue'])
+            }
+        };
+
+        TestBed.configureTestingModule({
+            declarations: [CkeditorPlaygroundComponent, TestDspTextValueAsXMLComponent],
+            providers: [
+                {
+                    provide: DspApiConnectionToken,
+                    useValue: dspSpyObj
+                }
+            ]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        const dspSpy = TestBed.inject(DspApiConnectionToken);
+
+        (dspSpy.v2.auth as jasmine.SpyObj<AuthenticationEndpointV2>).login.and.returnValue(
+            of({} as ApiResponseData<LoginResponse>)
+        );
+
+        (dspSpy.v2.res as jasmine.SpyObj<ResourcesEndpointV2>).getResource.and.returnValue(of(new ReadResource()));
+
+        fixture = TestBed.createComponent(CkeditorPlaygroundComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
