de914ead8c015ccedcd4f943b7708f690e2bf54d
flaurens <flavie.laurens@unibas.ch>
2020-10-06 14:52:18 +0200

    test(action): fix unit tests

--- projects/dsp-ui/src/lib/action/directives/external-links/external-links.directive.spec.ts
+++ projects/dsp-ui/src/lib/action/directives/external-links/external-links.directive.spec.ts
@@ -1,8 +1,55 @@
+import { Component, DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { ExternalLinksDirective } from './external-links.directive';
 
+@Component({
+    template: `<a href="http://dasch.swiss/">{{ label }}</a>`
+})
+class TestExternalLinkComponent {
+
+    label = 'DaSCH website';
+
+    constructor() { }
+}
+
 describe('ExternalLinksDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ExternalLinksDirective();
-    expect(directive).toBeTruthy();
-  });
+
+    let component: TestExternalLinkComponent;
+    let fixture: ComponentFixture<TestExternalLinkComponent>;
+    let linkEl: DebugElement;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                ExternalLinksDirective,
+                TestExternalLinkComponent
+            ]
+        });
+
+        fixture = TestBed.createComponent(TestExternalLinkComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+
+        linkEl = fixture.debugElement.query(By.css('a[href]'));
+
+    });
+
+    it('should create an instance', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should get the correct attributes', () => {
+        let relAttribute;
+        let targetAttribute;
+        let hrefAttribute;
+
+        relAttribute = linkEl.attributes.rel;
+        targetAttribute = linkEl.attributes.target;
+        hrefAttribute = linkEl.attributes.href;
+
+        expect(relAttribute).toEqual('noopener');
+        expect(targetAttribute).toEqual('_blank');
+        expect(hrefAttribute).toEqual('http://dasch.swiss/');
+    });
 });
