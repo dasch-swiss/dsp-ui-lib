b7896a284fbcb99df1665f0997955d74d95d7800
Marcin Procyk <marcin.procyk@dasch.swiss>
2020-10-26 14:23:00 +0100

    feat(viewer): add form buttons + enabled prettier formatting

--- projects/dsp-ui/src/lib/viewer/representation/upload-form/upload-form.component.spec.ts
+++ projects/dsp-ui/src/lib/viewer/representation/upload-form/upload-form.component.spec.ts
@@ -1,4 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { UploadFormComponent } from './upload-form.component';
 
@@ -8,7 +12,13 @@ describe('UploadFormComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-        declarations: [ UploadFormComponent ]
+        declarations: [ UploadFormComponent ],
+        imports: [
+            BrowserAnimationsModule,
+            MatInputModule,
+            MatSnackBarModule,
+        ],
+        providers: [ FormBuilder ]
         })
         .compileComponents();
     }));
