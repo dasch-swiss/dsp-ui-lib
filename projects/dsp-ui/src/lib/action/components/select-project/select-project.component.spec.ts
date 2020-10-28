3583ac5cb77712455924111468478b11a057ba7f
Snehal Kumbhar <snehal.kumbhar@unil.ch>
2020-10-28 12:17:30 +0100

    Added ubit tests for select-project component

--- projects/dsp-ui/src/lib/action/components/select-project/select-project.component.spec.ts
+++ projects/dsp-ui/src/lib/action/components/select-project/select-project.component.spec.ts
@@ -1,25 +1,126 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { Component, ViewChild } from '@angular/core';
+import { async, ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatAutocompleteHarness } from '@angular/material/autocomplete/testing';
+import { MatOptionModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MockProjects, ProjectsEndpointAdmin, UsersEndpointAdmin } from '@dasch-swiss/dsp-js';
+import { of } from 'rxjs/internal/observable/of';
+import { DspApiConnectionToken } from '../../../core';
 
 import { SelectProjectComponent } from './select-project.component';
 
+/**
+ * Test host component to simulate parent component.
+ */
+@Component({
+    selector: `dsp-select-project-host-component`,
+    template: `
+        <dsp-select-project #selectProject
+            [userIri]="userIri"
+            (projectInfo)="getProjectInfo($event)">
+        </dsp-select-project>
+    `
+})
+class TestHostSelectProjectComponent {
+
+    @ViewChild('selectProject') selectProject: SelectProjectComponent;
+
+    // userIRI filter for select-project component
+    userIri = 'http://rdfh.ch/users/9XBCrDV3SRa7kS1WwynB4Q';
+
+    selectedProjectCode: string;
+
+    getProjectInfo(projectName: string) {
+        this.selectedProjectCode = projectName;
+    }
+
+}
+
 describe('SelectProjectComponent', () => {
-  let component: SelectProjectComponent;
-  let fixture: ComponentFixture<SelectProjectComponent>;
+    let testHostComponent: TestHostSelectProjectComponent;
+    let testHostFixture: ComponentFixture<TestHostSelectProjectComponent>;
+    let dspConnSpy;
+    let loader: HarnessLoader;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SelectProjectComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SelectProjectComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        dspConnSpy = {
+            admin: {
+                // projectsEndpoint: jasmine.createSpyObj('projectsEndpoint', ['getProjects']),
+                usersEndpoint: jasmine.createSpyObj('usersEndpoint', ['getUserProjectMemberships'])
+            }
+        };
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        TestBed.configureTestingModule({
+            declarations: [
+                SelectProjectComponent,
+                TestHostSelectProjectComponent
+            ],
+            imports: [
+                OverlayModule,
+                FormsModule,
+                ReactiveFormsModule,
+                MatInputModule,
+                MatIconModule,
+                MatAutocompleteModule,
+                MatSelectModule,
+                MatOptionModule,
+                BrowserAnimationsModule
+            ],
+            providers: [
+                {
+                    provide: DspApiConnectionToken,
+                    useValue: dspConnSpy
+                }
+            ]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+
+        // mock getProjects response
+        const valuesSpy = TestBed.inject(DspApiConnectionToken);
+
+        (valuesSpy.admin.usersEndpoint as jasmine.SpyObj<UsersEndpointAdmin>).getUserProjectMemberships.and.callFake(
+            () => {
+                const projects = MockProjects.mockProjects();
+                return of(projects);
+            }
+        );
+
+        testHostFixture = TestBed.createComponent(TestHostSelectProjectComponent);
+        testHostComponent = testHostFixture.componentInstance;
+        loader = TestbedHarnessEnvironment.loader(testHostFixture);
+        testHostFixture.detectChanges();
+    });
+
+    fit('should create', () => {
+        expect(testHostComponent.selectProject).toBeTruthy();
+    });
+
+    it('should get projects for given user on init', () => {
+        const projSpy = TestBed.inject(DspApiConnectionToken);
+        expect(projSpy.admin.usersEndpoint.getUserProjectMemberships).toHaveBeenCalledTimes(1);
+
+        expect(testHostComponent.selectProject.projects).toBeDefined();
+        expect(testHostComponent.selectProject.projects.length).toEqual(8);
+    });
+
+    fit('should get filtered list of projects based on input', async () => {
+        const autoCompleteHarness = await loader.getHarness(MatAutocompleteHarness);
+        await autoCompleteHarness.enterText('knora');
+        const options = await autoCompleteHarness.getOptions();
+
+        expect(options.length).toEqual(2);
+        expect(await options[0].getText()).withContext('DefaultSharedOntologiesProject');
+    });
 });
