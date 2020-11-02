381ced06fa1524d826c5761ddbdc0133c7e2a3de
Marcin Procyk <marcin.procyk@dasch.swiss>
2020-11-02 14:54:44 +0100

    test(viewer): add basic test for UploadFormComponent

--- projects/dsp-ui/src/lib/viewer/representation/upload-form/upload-form.component.spec.ts
+++ projects/dsp-ui/src/lib/viewer/representation/upload-form/upload-form.component.spec.ts
@@ -1,28 +1,32 @@
-import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UploadFileService } from '../../services/upload-file.service';
 
 import { UploadFormComponent } from './upload-form.component';
 
 describe('UploadFormComponent', () => {
     let component: UploadFormComponent;
     let fixture: ComponentFixture<UploadFormComponent>;
+    class MockUploadFileService {
+        envUrl = 'envUrl';
+    }
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-        declarations: [ UploadFormComponent ],
-        imports: [
-            BrowserAnimationsModule,
-            HttpClientModule,
-            MatInputModule,
-            MatSnackBarModule
-        ],
-        providers: [ FormBuilder ]
+            declarations: [UploadFormComponent],
+            imports: [
+                MatInputModule,
+                MatSnackBarModule
+            ],
+            providers: [
+                UploadFormComponent,
+                FormBuilder,
+                { provide: UploadFileService, useClass: MockUploadFileService }
+            ]
         })
-        .compileComponents();
+            .compileComponents();
     }));
 
     beforeEach(() => {
@@ -31,7 +35,48 @@ describe('UploadFormComponent', () => {
         fixture.detectChanges();
     });
 
-    // it('should create', () => {
-    //     expect(component).toBeTruthy();
-    // });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should display resource type', () => {
+        expect(component.resourceTyoe).toBeDefined();
+    });
+
+    it('should be created with no fille', () => {
+        expect(component.file).toBeFalsy();
+    });
+
+    describe('form', () => {
+        it('should create form group and file control', () => {
+            expect(component.form).toBeDefined();
+            expect(component.fileControl).toBeTruthy();
+        });
+
+        it('should reset the form', () => {
+            const fb = new FormBuilder();
+            component.form = fb.group({ test: '' });
+            component.resetForm();
+            expect(component.form.get('test').value).toBeNull();
+        });
+    });
+
+    describe('isFileTypeSupported', () => {
+        it('should return true for the supported image files', () => {
+            const fileTypes = ['image/jpeg', 'image/jp2', 'image/tiff', 'image/tiff-fx', 'image/png'];
+            for (const type of fileTypes) {
+                expect(component.isFileTypeSupported(type)).toBeTruthy();
+            }
+
+        });
+
+        it('should return false for unsupported image files', () => {
+            // TODO: add real unsupported filetypes?
+            const fileTypes = ['image/a', 'image/b', 'image/c', 'image/d', 'image/e'];
+            for (const type of fileTypes) {
+                expect(component.isFileTypeSupported(type)).toBeFalsy();
+            }
+
+        });
+    });
 });
