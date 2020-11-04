ff442288e80384c5345ddb7d97047c7a8e583699
Tobias Schweizer <t.schweizer@unibas.ch>
2020-11-04 16:43:05 +0100

    tests (viewer): use httptestingcontroller in upload service test

--- projects/dsp-ui/src/lib/viewer/services/upload-file.service.spec.ts
+++ projects/dsp-ui/src/lib/viewer/services/upload-file.service.spec.ts
@@ -1,7 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
+import { async, TestBed } from '@angular/core/testing';
 import { AppInitService } from '../../core/app-init.service';
 import { Session, SessionService } from '../../core/session.service';
 import { UploadedFileResponse, UploadFileService } from './upload-file.service';
@@ -9,35 +7,35 @@ import { UploadedFileResponse, UploadFileService } from './upload-file.service';
 describe('UploadFileService', () => {
     let service: UploadFileService;
     let httpTestingController: HttpTestingController;
-    let httpClient: HttpClient;
-
-    let initServiceSpy: jasmine.SpyObj<AppInitService>;
-    let httpClientSpy: jasmine.SpyObj<HttpClient>;
-    let sessionServiceSpy: jasmine.SpyObj<SessionService>;
 
     const file = new File(['1'], 'testfile');
     const mockUploadData = new FormData();
     mockUploadData.append('test', file);
 
     beforeEach(() => {
-        const initSpy = jasmine.createSpyObj('AppInitService', ['config']);
-        const httpClientSpyObj = jasmine.createSpyObj('HttpClient', ['post']);
-        const sessionSpy = jasmine.createSpyObj('SessionService', ['getSession', 'setSession']);
+
+        const appInitSpy = {
+            config: {
+                sipiUrl: 'https://sipi.dasch.swiss/'
+            }
+        };
+
+        const sessionSpy = jasmine.createSpyObj('SessionService', ['getSession']);
 
         TestBed.configureTestingModule({
-            imports: [HttpClientTestingModule],
+            imports: [
+                HttpClientTestingModule
+            ],
             providers: [
-                { provide: AppInitService, useValue: initSpy },
-                { provide: HttpClient, useValue: httpClientSpyObj },
+                { provide: AppInitService, useValue: appInitSpy },
                 { provide: SessionService, useValue: sessionSpy },
             ]
         });
+
         service = TestBed.inject(UploadFileService);
         httpTestingController = TestBed.inject(HttpTestingController);
-        httpClient = TestBed.inject(HttpClient);
-        httpClientSpy = TestBed.inject(HttpClient) as jasmine.SpyObj<HttpClient>;
-        initServiceSpy = TestBed.inject(AppInitService) as jasmine.SpyObj<AppInitService>;
-        sessionServiceSpy = TestBed.inject(SessionService) as jasmine.SpyObj<SessionService>;
+
+        const sessionServiceSpy = TestBed.inject(SessionService) as jasmine.SpyObj<SessionService>;
 
         sessionServiceSpy.getSession.and.callFake(
             () => {
@@ -57,18 +55,17 @@ describe('UploadFileService', () => {
         );
     });
 
+    afterEach(() => {
+        // After every test, assert that there are no more pending requests.
+        httpTestingController.verify();
+    });
+
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
 
-    it('should call the upload() method', () => {
-        service = new UploadFileService(initServiceSpy, httpClientSpy as any, sessionServiceSpy);
-        service.upload(mockUploadData);
-        expect(httpClientSpy.post).toHaveBeenCalled();
-        expect(httpClientSpy.post).toHaveBeenCalledTimes(1);
-    });
+    it('should return expected file response on upload', done => {
 
-    it('should return expected file resposne (HttpClient called once)', () => {
         const expectedResponse: UploadedFileResponse = {
             uploadedFiles: [{
                 fileType: 'image',
@@ -78,12 +75,20 @@ describe('UploadFileService', () => {
             }]
         };
 
-        httpClientSpy.post.and.returnValue(of(expectedResponse));
-
         service.upload(mockUploadData).subscribe(
-            res => expect(res).toEqual(expectedResponse, 'expected files response'),
-            fail
+            res => {
+                expect(res.uploadedFiles.length).toEqual(1);
+                expect(res.uploadedFiles[0].internalFilename).toEqual('8R0cJE3TSgB-BssuQyeW1rE.jp2');
+                done();
+            }
         );
-        expect(httpClientSpy.post.calls.count()).toBe(1, 'one call');
+
+        const httpRequest = httpTestingController.expectOne('https://sipi.dasch.swiss/upload?token=myToken');
+
+        expect(httpRequest.request.method).toEqual('POST');
+
+        httpRequest.flush(expectedResponse);
+
     });
+
 });
