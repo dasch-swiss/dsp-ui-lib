ce738efb2dcf65f9ecd2cbf583241cd49968cd94
Snehal Kumbhar <snehal.kumbhar@unil.ch>
2020-11-25 10:33:17 +0100

    DSP-945-update resource viewer (#230)
    * DSP-945-update resource viewer
    
    * enhancement (resource-grid-viewer): highlight selected item from grid, by defailt select first item.
    * enhancement (resource-list-viewer): highlight selected item from list, by default select first item.
    
    Co-authored-by: André Kilchenmann <a.kilchenmann@dasch.swiss>

--- projects/dsp-ui/src/lib/viewer/views/list-view/resource-grid/resource-grid.component.ts
+++ projects/dsp-ui/src/lib/viewer/views/list-view/resource-grid/resource-grid.component.ts
@@ -15,6 +15,8 @@ export class ResourceGridComponent {
      */
     @Input() resources: ReadResourceSequence;
 
+    @Input() selectedResourceIdx: number;
+
     /**
      * Click on an item will emit the resource iri
      *
