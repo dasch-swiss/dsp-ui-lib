e9b6b1d5db20c208d60487c24bca709a842d1e77
André Kilchenmann <a.kilchenmann@dasch.swiss>
2020-12-02 12:16:21 +0100

    DSP-1110 Bug fixes and styling improvement in resource viewer (#237)
    * style(viewer): Fix highlight style in search results
    
    * refactor(viewer): Support click and hover on project link
    
    * refactor(viewer): Fix issue with username in prop toolbar
    
    * refactor(viewer): Improvements and bug fix on refresh
    
    * refactor(viewer): Consistent @outputs; resolves DSP-1023
    
    * fix(viewer): Reset still image view on change
    
    * feat(playground): New examples for resource viewer
    
    * chore(viewer): Delete wrong console.log

--- src/app/viewer-playground/viewer-playground.component.ts
+++ src/app/viewer-playground/viewer-playground.component.ts
@@ -10,7 +10,14 @@ import { ReadLinkValue, ReadProject } from '@dasch-swiss/dsp-js';
 export class ViewerPlaygroundComponent implements OnInit {
 
     // resorce from incunabula: book page
-    resource = 'http://rdfh.ch/0803/18a671b8a601';
+    resources = [
+        'http://rdfh.ch/0803/18a671b8a601',
+        'http://rdfh.ch/0803/7e4cfc5417',
+        'http://rdfh.ch/0803/6ad3e2c47501',
+        'http://rdfh.ch/0001/a-thing-with-text-valuesLanguage'
+    ];
+    resourceIri: string = this.resources[0];
+
 
     constructor(
         private _sessionService: SessionService
@@ -20,9 +27,13 @@ export class ViewerPlaygroundComponent implements OnInit {
         this._sessionService.isSessionValid().subscribe(status => console.log('session valid: ', status));
     }
 
-    openProject(project: ReadProject) {
+    refProjectClicked(project: ReadProject) {
         // here you can redirect a user to the project page
-        console.log('redircet to project page e.g. /project/:shortcode', project.shortcode);
+        console.log('project clicked: redircet to project page e.g. /project/:shortcode', project);
+    }
+
+    refProjectHovered(project: ReadProject) {
+        console.log('project hovered: show preview', project);
     }
 
     refResourceClicked(linkValue: ReadLinkValue) {
