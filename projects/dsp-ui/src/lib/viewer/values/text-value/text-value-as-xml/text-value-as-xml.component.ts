1b3a535cbf76ac0c5f7799f4c35b8768b8c796d5
mdelez <60604010+mdelez@users.noreply.github.com>
2020-12-16 15:40:34 +0100

    refactor (text-value-as-xml): refactor and clean up text-value-as-xml value

--- projects/dsp-ui/src/lib/viewer/values/text-value/text-value-as-xml/text-value-as-xml.component.ts
+++ projects/dsp-ui/src/lib/viewer/values/text-value/text-value-as-xml/text-value-as-xml.component.ts
@@ -138,7 +138,7 @@ export class TextValueAsXMLComponent extends BaseValueComponent implements OnIni
         );
 
         this.form = this.fb.group({
-            xmlValue: this.valueFormControl,
+            value: this.valueFormControl,
             comment: this.commentFormControl
         });
 
