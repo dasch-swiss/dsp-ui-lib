d79046dcbff7eedd4e2e53e74fac11032253e9d2
Tobias Schweizer <t.schweizer@unibas.ch>
2021-06-03 17:19:38 +0200

    test(date edit): add spec

--- projects/dsp-ui/src/lib/viewer/values/date-value/date-input-text/date-edit/date-edit.component.spec.ts
+++ projects/dsp-ui/src/lib/viewer/values/date-value/date-input-text/date-edit/date-edit.component.spec.ts
@@ -1,27 +1,97 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DateEditComponent } from './date-edit.component';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { KnoraDate } from '@dasch-swiss/dsp-js';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatRadioModule } from '@angular/material/radio';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputHarness } from '@angular/material/input/testing';
+import { MatSelectHarness } from '@angular/material/select/testing';
 
-/*
+/**
+ * Test host component to simulate parent component.
+ */
+@Component({
+    template: `
+        <div [formGroup]="form">
+            <mat-form-field>
+                <dsp-date-edit #dateEdit [formControlName]="'date'" [calendar]="calendar"></dsp-date-edit>
+            </mat-form-field>
+        </div>`
+})
+class TestHostComponent implements OnInit {
+
+    @ViewChild('dateEdit') dateEditComponent: DateEditComponent;
+
+    form: FormGroup;
+
+    calendar = 'GREGORIAN';
+
+    constructor(private _fb: FormBuilder) {
+    }
+
+    ngOnInit(): void {
+
+        this.form = this._fb.group({
+            date: [new KnoraDate('JULIAN', 'CE', 2018, 5, 19)]
+        });
+
+    }
+}
+
 describe('DateEditComponent', () => {
-  let component: DateEditComponent;
-  let fixture: ComponentFixture<DateEditComponent>;
+    let testHostComponent: TestHostComponent;
+    let testHostFixture: ComponentFixture<TestHostComponent>;
+    let loader: HarnessLoader;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DateEditComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                ReactiveFormsModule,
+                MatFormFieldModule,
+                MatInputModule,
+                MatRadioModule,
+                BrowserAnimationsModule,
+                MatSelectModule
+            ],
+            declarations: [DateEditComponent, TestHostComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DateEditComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        testHostFixture = TestBed.createComponent(TestHostComponent);
+        testHostComponent = testHostFixture.componentInstance;
+        loader = TestbedHarnessEnvironment.loader(testHostFixture);
+        testHostFixture.detectChanges();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        expect(testHostComponent).toBeTruthy();
+    });
+
+    it('should initialize a date correctly', async () => {
+
+        expect(testHostComponent.dateEditComponent.calendar).toEqual('GREGORIAN');
+
+        expect(testHostComponent.dateEditComponent.yearControl.value).toEqual(2018);
+        expect(testHostComponent.dateEditComponent.monthControl.value).toEqual(5);
+        expect(testHostComponent.dateEditComponent.dayControl.value).toEqual(19);
+
+        const yearInput = await loader.getHarness(MatInputHarness.with({selector: '.year'}));
+        expect(await yearInput.getValue()).toEqual('2018');
+
+        const monthInput = await loader.getHarness(MatSelectHarness.with({selector: '.month'}));
+        expect(await monthInput.getValueText()).toEqual('5');
+
+        const dayInput = await loader.getHarness(MatSelectHarness.with({selector: '.day'}));
+        expect(await dayInput.getValueText()).toEqual('19');
+
+    });
+
 });
-*/
+
