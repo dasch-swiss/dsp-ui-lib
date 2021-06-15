11b8a6023ea653adb91be71ab5a9e6ab4f25723e
Tobias Schweizer <t.schweizer@unibas.ch>
2021-06-15 15:59:06 +0200

    test(viewer): add specs to geoname service

--- projects/dsp-ui/src/lib/viewer/services/geoname.service.ts
+++ projects/dsp-ui/src/lib/viewer/services/geoname.service.ts
@@ -2,18 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
+import { AppInitService } from '../../core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GeonameService {
 
-  constructor(private readonly _http: HttpClient) {
+  constructor(
+      private readonly _http: HttpClient,
+      private _appInitService: AppInitService
+  ) {
   }
 
   resolveGeonameID(id: string): Observable<string> {
 
-      return this._http.get<object>('https://ws.geonames.net/getJSON?geonameId=' + id + '&username=knora&style=long').pipe(
+      return this._http.get<object>('https://ws.geonames.net/getJSON?geonameId=' + id + '&username=' + this._appInitService.config['geonameToken'] + '&style=short').pipe(
           map(
               (geo: { name: string }) => {
 
